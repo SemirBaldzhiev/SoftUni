80f823e94a520945fb1f0756b8ba72c2a7f6b1a9
Semir Baldzhiev <semir.baldzhiev@gmail.com>
2020-09-21 22:38:53 +0300

    Exercises

---
+++ C# Fundamentals/2.1 DataTypesAndVariables - Exercise/02. SumDigits/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _02._SumDigits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string number = Console.ReadLine();
+
+            int length = number.Length;
+            int sumDigits = 0;
+
+            for (int i = 0; i < length; i++)
+            {
+                sumDigits += int.Parse(number[i].ToString());
+            }
+
+            Console.WriteLine(sumDigits);
+        }
+    }
+}
