4a9038bbf3a282a5ef1ae1cdb84077716c7f375e
Semir Baldzhiev <semir.baldzhiev@gmail.com>
2020-09-22 23:18:49 +0300

    Exercise

--- C# Fundamentals/2.1 DataTypesAndVariables - Exercise/07. WaterOverflow/Program.cs
+++ C# Fundamentals/2.1 DataTypesAndVariables - Exercise/07. WaterOverflow/Program.cs
@@ -6,7 +6,27 @@ namespace _07._WaterOverflow
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int n = int.Parse(Console.ReadLine());
+
+            int sumLiters = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                int liters = int.Parse(Console.ReadLine());
+                sumLiters += liters;
+
+                if (sumLiters <= 255)
+                {
+                    continue;
+                }
+                else
+                {
+                    sumLiters -= liters;
+                    Console.WriteLine("Insufficient capacity!");
+                }
+            }
+
+            Console.WriteLine(sumLiters);
         }
     }
 }
---
+++ C# Fundamentals/2.1 DataTypesAndVariables - Exercise/08. BeerKegs/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _08._BeerKegs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            double biggestKeg = 0;
+            string biggestKegModel = string.Empty;
+
+            for (int i = 0; i < n; i++)
+            {
+                string model = Console.ReadLine();
+                double radius = double.Parse(Console.ReadLine());
+                int height = int.Parse(Console.ReadLine());
+
+                double kegVolume = Math.PI * radius * radius * height;
+
+                if (kegVolume > biggestKeg)
+                {
+                    biggestKeg = kegVolume;
+                    biggestKegModel = model;
+                }
+            }
+
+            Console.WriteLine(biggestKegModel);
+        }
+    }
+}
