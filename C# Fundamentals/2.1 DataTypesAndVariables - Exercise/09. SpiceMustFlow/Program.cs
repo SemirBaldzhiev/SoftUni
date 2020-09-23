9503b3a543e31534a357393ce1ec5ca1c11c27c8
Semir Baldzhiev <semir.baldzhiev@gmail.com>
2020-09-23 23:39:50 +0300

    Exercise

---
+++ C# Fundamentals/2.1 DataTypesAndVariables - Exercise/09. SpiceMustFlow/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _09._SpiceMustFlow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int startingYield = int.Parse(Console.ReadLine());
+            int totalAmount = 0;
+            int days = 0;
+
+
+            if (startingYield >= 100)
+            {
+                while (startingYield >= 100)
+                {
+                    totalAmount += (startingYield - 26);
+
+                    startingYield -= 10;
+                    days++;
+                }
+
+                totalAmount -= 26;
+            }
+           
+            Console.WriteLine(days);
+            Console.WriteLine(totalAmount);
+        }
+    }
+}
