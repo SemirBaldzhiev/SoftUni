ac024c59af7de0d51c8916588c4a29e55e5d4bf9
Semir Baldzhiev <semir.baldzhiev@gmail.com>
2020-10-01 21:12:57 +0300

    Exercise

--- C# Fundamentals/3 Arrays - Exercise/08. MagicSum/Program.cs
+++ C# Fundamentals/3 Arrays - Exercise/08. MagicSum/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace _08._MagicSum
 {
@@ -6,7 +7,24 @@ namespace _08._MagicSum
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int num = int.Parse(Console.ReadLine());
+
+            int length = numbers.Length;
+
+            for (int i = 0; i < length; i++)
+            {
+                for (int j = i + 1; j < length; j++)
+                {
+                    if (numbers[i] + numbers[j] == num)
+                    {
+                        Console.WriteLine($"{numbers[i]} {numbers[j]}");
+                    }
+                }
+
+                
+            }
+
         }
     }
 }
