a0660485e23682764f42cef8f70c8e273dc9ee17
Semir Baldzhiev <semir.baldzhiev@gmail.com>
2020-10-10 20:06:56 +0300

    Exercise

---
+++ C# Fundamentals/4. Methods - Exercise/03. CharactersInRange/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _03._CharactersInRange
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char firstChar = char.Parse(Console.ReadLine());
+            char secondChar = char.Parse(Console.ReadLine());
+
+            BetweenCharInAscii(firstChar, secondChar);
+        }
+
+        private static void BetweenCharInAscii(char firstChar, char secondChar)
+        {
+            int firstCharAscii = (int)firstChar;
+            int secondCharAscii = (int)secondChar;
+
+            for (int i = firstCharAscii + 1; i < secondCharAscii; i++)
+            {
+                Console.WriteLine($"{(char)i} ");
+            }
+        }
+    }
+}
