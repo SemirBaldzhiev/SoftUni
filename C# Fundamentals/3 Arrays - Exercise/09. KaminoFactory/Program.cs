2032b5f8d2c39c318301104ceee1f0ef44f901de
Semir Baldzhiev <semir.baldzhiev@gmail.com>
2020-10-10 14:32:50 +0300

    Exercise

---
+++ C# Fundamentals/3 Arrays - Exercise/09. KaminoFactory/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Linq;
+
+namespace _09._KaminoFactory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int dnaLength = int.Parse(Console.ReadLine());
+            int[] dna = new int[dnaLength];
+            int bestSequenceIndex = 0;
+            int bestSequenceCount = 0;
+            int bestSum = 0;
+            int[] bestDna = new int[dnaLength];
+            int index = 0;
+            int bestIndex = 0;
+
+
+            while (true)
+            {
+                string line = Console.ReadLine();
+
+                if (line == "Clone them!")
+                {
+                    Console.WriteLine($"Best DNA sample {bestIndex} with sum: {bestSum}.");
+                    Console.WriteLine(string.Join(" ", bestDna));
+                    break;
+                }
+
+                dna = line.Split("!", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+                index++;
+
+                int sequenceIndex = 0;
+                int sequenceCount = 1;
+                int sum = dna.Sum();
+
+                int maxSequenceCount = 0;
+
+                
+                for (int i = 0; i < dnaLength - 1; i++)
+                {
+                   
+                    if (dna[i] == dna[i + 1] && dna[i] == 1)
+                    {
+                        sequenceCount++;
+                    }
+                    else
+                    {
+                        sequenceCount = 1;
+                    }
+
+                    if (sequenceCount > maxSequenceCount)
+                    {
+                        maxSequenceCount = sequenceCount;
+                        sequenceIndex = i;
+                    }
+                    
+
+                }
+
+                if (maxSequenceCount > bestSequenceCount)
+                {
+                    bestSequenceCount = maxSequenceCount;
+                    bestSequenceIndex = sequenceIndex;
+                    bestDna = dna;
+                    bestIndex = index;
+                    bestSum = sum;
+                }
+                else if (maxSequenceCount == bestSequenceCount)
+                {
+                    if (sequenceIndex < bestSequenceIndex)
+                    {
+                        bestDna = dna;
+                        bestIndex = index;
+                        bestSum = sum;
+                    }
+                    else if (sequenceIndex == bestSequenceIndex)
+                    {
+                        if (sum > bestSum)
+                        {
+                            bestDna = dna;
+                            bestIndex = index;
+                            bestSum = sum;
+                        }
+                    }
+                }
+
+
+            }
+
+
+        }
+    }
+}
---
+++ C# Fundamentals/4. Methods - Exercise/1. SmallestOfThreeNumbers/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _1._SmallestOfThreeNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+
+            PrintSmallestInteger(a, b, c);
+        }
+
+        private static void PrintSmallestInteger(int a, int b, int c)
+        {
+            if (a < b && a < c)
+            {
+                Console.WriteLine(a);
+            }
+            else if (b < c && b < a)
+            {
+                Console.WriteLine(b);
+            }
+            else
+            {
+                Console.WriteLine(c);
+            }
+        }
+    }
+}
