8e73342efea5eb8251c89e0edd74d08e5d6129f7
Semir Baldzhiev <semir.baldzhiev@gmail.com>
2020-10-10 14:51:11 +0300

    Exercise

---
+++ C# Fundamentals/4. Methods - Exercise/02. VowelsCount/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _02._VowelsCount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            input = input.ToLower();
+            int countVowels = VowelsCount(input);
+
+            Console.WriteLine(countVowels);
+        }
+
+        private static int VowelsCount(string input)
+        {
+            int length = input.Length;
+            int count = 0;
+            for (int i = 0; i < length; i++)
+            {
+                if (input[i] == 'a' || input[i] == 'u' || input[i] == 'i' || input[i] == 'e' || input[i] == 'o')
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+    }
+}
