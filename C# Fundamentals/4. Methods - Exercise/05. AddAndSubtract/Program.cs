d11f518cf0bff1e7b8316bdc4b5f63016d1ba984
Semir Baldzhiev <semir.baldzhiev@gmail.com>
2020-10-12 22:09:06 +0300

    Exercise

---
+++ C# Fundamentals/4. Methods - Exercise/05. AddAndSubtract/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _05._AddAndSubtract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNum = int.Parse(Console.ReadLine());
+            int secondNum = int.Parse(Console.ReadLine());
+            int thirdNum = int.Parse(Console.ReadLine());
+
+            int sumNums= Sum(firstNum, secondNum);
+            int subtractNums = Subtract(sumNums, thirdNum);
+
+            Console.WriteLine(subtractNums);
+
+        }
+
+        private static int Subtract(int sumNums, int thirdNum)
+        {
+            return sumNums - thirdNum;
+        }
+
+        private static int Sum(int firstNum, int secondNum)
+        {
+            return firstNum + secondNum;
+        }
+    }
+}
