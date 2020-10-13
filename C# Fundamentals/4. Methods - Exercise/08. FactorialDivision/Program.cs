36e7c9da30b67397fc2c98dcea1932a9d184d655
Semir Baldzhiev <semir.baldzhiev@gmail.com>
2020-10-13 20:59:32 +0300

    Exercise

---
+++ C# Fundamentals/4. Methods - Exercise/08. FactorialDivision/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _08._FactorialDivision
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNum = int.Parse(Console.ReadLine());
+            int secondNum = int.Parse(Console.ReadLine());
+
+            int factorialFirstNum = Fatctorial(firstNum);
+            int factorialSecondNum = Fatctorial(secondNum);
+
+            double division = factorialFirstNum / (double)factorialSecondNum;
+
+            Console.WriteLine($"{division:f2}");
+
+        }
+
+        private static int Fatctorial(int firstNum)
+        {
+            int fact = 1;
+
+            for (int i = 2; i <= firstNum; i++)
+            {
+                fact *= i;
+            }
+
+            return fact;
+        }
+    }
+}
