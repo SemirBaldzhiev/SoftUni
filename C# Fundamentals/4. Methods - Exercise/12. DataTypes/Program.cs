6bf9a1fd99f9904466cc64b5313acac39045031d
Semir Baldzhiev <semir.baldzhiev@gmail.com>
2020-10-15 18:11:59 +0300

    Exercise

---
+++ C# Fundamentals/4. Methods - Exercise/12. DataTypes/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace _12._DataTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string dataType = Console.ReadLine();
+
+            if (dataType == "int")
+            {
+                int num = int.Parse(Console.ReadLine());
+                Multiply(num);
+            }
+            else if (dataType == "double")
+            {
+                double num = double.Parse(Console.ReadLine());
+                Multiply(num);
+            }
+            else if (dataType == "string")
+            {
+                string text = Console.ReadLine();
+                Surround(text);
+            }
+        }
+
+        private static void Surround(string text)
+        {
+            Console.WriteLine($"${text}$");
+        }
+
+        private static void Multiply(int num)
+        {
+            Console.WriteLine(num * 2);
+        }
+        private static void Multiply(double num)
+        {
+            Console.WriteLine($"{(num * 1.5):f2}");
+        }
+    }
+}
