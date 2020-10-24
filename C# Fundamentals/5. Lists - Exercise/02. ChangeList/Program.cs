d28aa7143696ca411b44e44813c681dfac4cfff6
Semir Baldzhiev <semir.baldzhiev@gmail.com>
2020-10-24 16:21:40 +0300

    Exercise

---
+++ C# Fundamentals/5. Lists - Exercise/02. ChangeList/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02._ChangeList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+            string line = Console.ReadLine();
+
+            while (line != "end")
+            {
+                string[] commands = line.Split().ToArray();
+                int element = int.Parse(commands[1]);
+                switch (commands[0])
+                {
+                    case "Delete":
+                        numbers.RemoveAll(x => x == element);
+                        break;
+                    case "Insert":
+                        int possition = int.Parse(commands[2]);
+                        numbers.Insert(possition, element);
+                        break;
+                }
+
+                line = Console.ReadLine();
+            }
+
+            Console.WriteLine(string.Join(' ', numbers));
+        }
+    }
+}
