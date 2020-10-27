fc1e9430d5829d59c7833a0421f1b5a4ae19b1b6
Semir Baldzhiev <semir.baldzhiev@gmail.com>
2020-10-27 17:26:11 +0200

    Exercise

---
+++ C# Fundamentals/5. Lists - Exercise/05. BombNumbers/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05._BombNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+            int[] bombNumbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int index = numbers.IndexOf(bombNumbers[0]);
+            int range = bombNumbers[1];
+
+            while (numbers.Contains(bombNumbers[0]))
+            {
+                int startIndex = index - range;
+                int endIndex = index + range;
+
+
+                if (startIndex < 0)
+                {
+                    startIndex = 0;
+                }
+
+                if (endIndex > numbers.Count - 1)
+                {
+                    endIndex = numbers.Count - 1;
+                }
+
+                int count = endIndex - startIndex + 1;
+
+                numbers.RemoveRange(startIndex, count);
+
+                index = numbers.IndexOf(bombNumbers[0]);
+            }
+            int sum = numbers.Sum();
+
+            Console.WriteLine(sum);
+        }
+    }
+}
