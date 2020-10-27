21d2f1bd88f73916ac1d5c5094ae32e10ee1a3f0
Semir Baldzhiev <semir.baldzhiev@gmail.com>
2020-10-27 18:58:50 +0200

    Exercise

---
+++ C# Fundamentals/5. Lists - Exercise/07. AppendArrays/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _07._AppendArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> numbers = Console.ReadLine().Split('|', StringSplitOptions.RemoveEmptyEntries).Reverse().ToList();
+
+            List<int> result = new List<int>();
+
+            foreach (var arr in numbers)
+            {
+                result.AddRange(arr
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToList());
+            }
+
+            Console.WriteLine(string.Join(' ', result));
+        }
+    }
+}
