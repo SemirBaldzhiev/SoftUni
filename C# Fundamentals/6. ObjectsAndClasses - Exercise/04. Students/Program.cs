d3be24b6ce95d41e8a6c4971a2c4a14f4c3b19b9
Semir Baldzhiev <semir.baldzhiev@gmail.com>
2020-10-30 14:58:05 +0200

    Exercise

---
+++ C# Fundamentals/6. ObjectsAndClasses - Exercise/04. Students/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04._Students
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+            List<Student> students = new List<Student>();
+
+            for (int i = 0; i < count; i++)
+            {
+                string[] studentInfo = Console.ReadLine().Split().ToArray();
+
+                Student student = new Student(studentInfo[0], studentInfo[1], double.Parse(studentInfo[2]));
+
+                students.Add(student);
+
+            }
+
+            foreach (var student in students.OrderByDescending(x => x.Grade))
+            {
+                Console.WriteLine(student.ToString());
+            }
+
+        }
+    }
+
+    public class Student
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public double Grade { get; set; }
+
+        public Student(string firstName, string lastName, double grade)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Grade = grade;
+        }
+
+        public override string ToString()
+        {
+            return $"{FirstName} {LastName}: {Grade:f2}";
+        }
+    }
+}
