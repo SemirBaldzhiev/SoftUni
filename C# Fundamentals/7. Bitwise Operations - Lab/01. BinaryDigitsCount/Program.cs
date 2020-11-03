06bc9995aeaffd8ade14c9469670f26b3c0ac201
Semir Baldzhiev <semir.baldzhiev@gmail.com>
2020-11-03 10:20:45 +0200

    Exercise

---
+++ C# Fundamentals/7. Bitwise Operations - Lab/01. BinaryDigitsCount/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _01._BinaryDigitsCount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
