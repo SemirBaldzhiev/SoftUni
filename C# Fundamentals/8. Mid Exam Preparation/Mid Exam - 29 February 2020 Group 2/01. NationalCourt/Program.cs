49bf0544a77c2f438117e30e82bfa0b6464afd73
Semir Baldzhiev <semir.baldzhiev@gmail.com>
2020-11-03 14:45:36 +0200

    Exam

---
+++ C# Fundamentals/8. Mid Exam Preparation/Mid Exam - 29 February 2020 Group 2/01. NationalCourt/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _01._NationalCourt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstEmployeePeople = int.Parse(Console.ReadLine());
+            int secondEmployeePeople = int.Parse(Console.ReadLine());
+            int thirdEmployeePeople = int.Parse(Console.ReadLine());
+            int peopleCount = int.Parse(Console.ReadLine());
+
+            int peopleForOneHour = firstEmployeePeople + secondEmployeePeople + thirdEmployeePeople;
+
+            int hours = 0;
+
+            while (peopleCount > 0)
+            {
+                peopleCount -= peopleForOneHour;
+                hours++;
+
+                if (hours % 4 == 0)
+                {
+                    hours++;
+                }
+                   
+            }
+
+            Console.WriteLine($"Time needed: {hours}h.");
+        }
+    }
+}
