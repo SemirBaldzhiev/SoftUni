5f149b8d6c098940534ce49b77cab34aa6151747
Semir Baldzhiev <semir.baldzhiev@gmail.com>
2020-11-03 16:53:54 +0200

    Exam

---
+++ C# Fundamentals/8. Mid Exam Preparation/Mid Exam - 29 February 2020 Group 2/02. ShoppingList/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02._ShoppingList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> groceryList = Console.ReadLine().Split('!').ToList();
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+
+                if (input == "Go Shopping!")
+                {
+                    Console.WriteLine(string.Join(", ", groceryList));
+                    break;
+                }
+
+                string[] commands = input.Split().ToArray();
+                string item = commands[1];
+
+                switch (commands[0])
+                {
+                    case "Urgent":
+                        if (!groceryList.Contains(item))
+                        {
+                            groceryList.Insert(0, item);
+                        }
+                        break;
+                    case "Unnecessary":
+                        if (groceryList.Contains(item))
+                        {
+                            groceryList.Remove(item);
+                        }
+                        break;
+                    case "Correct":
+                        if (groceryList.Contains(item))
+                        {
+                            string newItem = commands[2];
+                            int oldItemIndex = groceryList.FindIndex(x => x == item);
+
+                            groceryList[oldItemIndex] = newItem;
+                        }
+                        break;
+                    case "Rearrange":
+                        if (groceryList.Contains(item))
+                        {
+                            groceryList.Remove(item);
+                            groceryList.Add(item);
+                        }
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ C# Fundamentals/8. Mid Exam Preparation/Mid Exam - 29 February 2020 Group 2/03. HeartDelivery/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Linq;
+
+namespace _03._HeartDelivery
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] neighborhood = Console.ReadLine().Split('@').Select(int.Parse).ToArray();
+            int currentIndex = 0;
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+
+                if (input == "Love!")
+                {
+                    int houseCount = neighborhood.Count(x => x != 0);
+
+                    Console.WriteLine($"Cupid's last position was {currentIndex}.");
+                    if (neighborhood.All(x => x == 0))
+                    {
+                        Console.WriteLine("Mission was successful.");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Cupid has failed {houseCount} places.");
+                    }
+                    break;
+                }
+
+                string[] commands = input.Split().ToArray();
+
+                if (commands[0] == "Jump")
+                {
+                    int length = int.Parse(commands[1]);
+
+                    currentIndex += length;
+
+                    if (currentIndex >= neighborhood.Length)
+                    {
+                        currentIndex = 0;
+                    }
+                    
+                    if (neighborhood[currentIndex] == 0)
+                    {
+                        Console.WriteLine($"Place {currentIndex} already had Valentine's day.");
+                    }
+                    else
+                    {
+                        neighborhood[currentIndex] -= 2;
+
+                        if (neighborhood[currentIndex] == 0)
+                        {
+                            Console.WriteLine($"Place {currentIndex} has Valentine's day.");
+                        }
+                    }
+                    
+                }
+            }
+        }
+    }
+}
