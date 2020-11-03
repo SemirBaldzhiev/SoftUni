cd438b3f1b9136f29c3b0ae6bc39fde42b9c466d
Semir Baldzhiev <semir.baldzhiev@gmail.com>
2020-11-03 11:59:09 +0200

    Exam

--- C# Fundamentals/8. Mid Exam Preparation/Mid Exam - 07 April 2020/02. ShootForTheWin/Program.cs
+++ C# Fundamentals/8. Mid Exam Preparation/Mid Exam - 07 April 2020/02. ShootForTheWin/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _02._ShootForTheWin
 {
@@ -6,7 +8,45 @@ namespace _02._ShootForTheWin
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] targets = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int count = 0;
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+
+                if (input == "End")
+                {
+                    Console.WriteLine($"Shot targets: {count} -> {string.Join(' ', targets)}");
+                    break;
+                }
+
+                int index = int.Parse(input);
+
+                if ((index < 0 || index >= targets.Length) || targets[index] == -1)
+                {
+                    continue;
+                }
+
+                int targetValue = targets[index];
+                targets[index] = -1;
+                count++;
+
+                for (int i = 0; i < targets.Length; i++)
+                {
+                    if (targets[i] != -1)
+                    {
+                        if (targets[i] > targetValue)
+                        {
+                            targets[i] -= targetValue;
+                        }
+                        else if (targets[i] <= targetValue)
+                        {
+                            targets[i] += targetValue;
+                        }
+                    }
+                }
+             }
         }
     }
 }
