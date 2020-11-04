6379d84657115653f16435b12cfdc6f650fe50f2
Semir Baldzhiev <semir.baldzhiev@gmail.com>
2020-11-04 11:29:41 +0200

    Exam

---
+++ C# Fundamentals/8. Mid Exam Preparation/Mid Exam - 29 February 2020 Group 1/01. BonusScoringSystem/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _01._BonusScoringSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int countOfStudents = int.Parse(Console.ReadLine());
+            int lecturesCount = int.Parse(Console.ReadLine());
+            int additionalBonus = int.Parse(Console.ReadLine());
+
+            int maxAttendances = 0;
+
+            if (countOfStudents == 0 || lecturesCount == 0)
+            {
+                Console.WriteLine($"Max Bonus: 0.");
+                Console.WriteLine($"The student has attended 0 lectures.");
+                return;
+            }
+
+            for (int i = 0; i < countOfStudents; i++)
+            {
+                int attendances = int.Parse(Console.ReadLine());
+
+                if (attendances > maxAttendances)
+                {
+                    maxAttendances = attendances;
+                }
+            }
+
+            double maxBonus = Math.Ceiling(maxAttendances / (double)lecturesCount * (5 + additionalBonus));
+
+            Console.WriteLine($"Max Bonus: {maxBonus}.");
+            Console.WriteLine($"The student has attended {maxAttendances} lectures.");
+        }
+    }
+}
