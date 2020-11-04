ef8b66d4ad42837f46ca6793b7fc284821c9c5a0
Semir Baldzhiev <semir.baldzhiev@gmail.com>
2020-11-04 14:49:51 +0200

    Exam

---
+++ C# Fundamentals/8. Mid Exam Preparation/Mid Exam - 29 February 2020 Group 1/03. Inventory/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03._Inventory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> inventory = Console.ReadLine().Split(", ").ToList();
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+
+                if (input == "Craft!")
+                {
+                    Console.WriteLine(string.Join(", ", inventory));
+                    break;
+                }
+
+                string[] commands = input.Split(" - ").ToArray();
+
+                string item = commands[1];
+
+                switch (commands[0])
+                {
+                    case "Collect":
+                        if (!inventory.Contains(item))
+                        {
+                            inventory.Add(item);
+                        }
+                        break;
+                    case "Drop":
+                        if (inventory.Contains(item))
+                        {
+                            inventory.Remove(item);
+                        }
+                        break;
+                    case "Combine Items":
+
+                        string[] items = commands[1].Split(":").ToArray();
+                        string oldItem = items[0];
+                        string newItem = items[1];
+
+                        if (inventory.Contains(oldItem))
+                        {
+                            int oldItemIndex = inventory.FindIndex(x => x == oldItem);
+                            inventory.Insert(oldItemIndex + 1, newItem);
+                        }
+                        break;
+                    case "Renew":
+                        if (inventory.Contains(item))
+                        {
+                            inventory.Remove(item);
+                            inventory.Add(item);
+                        }
+                        break;
+                }
+            }
+        }
+    }
+}
