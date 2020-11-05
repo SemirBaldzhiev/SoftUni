b02fcd9db5f409d5ab5e0bdf1300e76dcf22a168
Semir Baldzhiev <semir.baldzhiev@gmail.com>
2020-11-05 09:04:39 +0200

    Exam

---
+++ C# Fundamentals/8. Mid Exam Preparation/Mid Exam - 5 July 2020/02. ArrayModifier/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Linq;
+
+namespace _02._ArrayModifier
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+
+                if (input == "end")
+                {
+                    Console.WriteLine(string.Join(", ", numbers));
+                    break;
+                }
+
+                string[] commands = input.Split().ToArray();
+
+                int firstIndex = 0;
+                int secondIndex = 0;
+                
+                if (commands.Length == 3)
+                {
+                    firstIndex = int.Parse(commands[1]);
+                    secondIndex = int.Parse(commands[2]);
+                }
+
+                switch (commands[0])
+                {
+                    case "swap":
+                        int temp = numbers[firstIndex];
+                        numbers[firstIndex] = numbers[secondIndex];
+                        numbers[secondIndex] = temp;
+                        break;
+                    case "multiply":
+                        int result = numbers[firstIndex] * numbers[secondIndex];
+                        numbers[firstIndex] = result;
+                        break;
+                    case "decrease":
+                        numbers = numbers.Select(x => x - 1).ToArray();
+                        break;
+                }
+
+
+            }
+
+        }
+    }
+}
