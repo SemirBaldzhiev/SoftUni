9ad71691c3839c2205c9e9a3b26c2bffabba5233
Semir Baldzhiev <semir.baldzhiev@gmail.com>
2020-11-05 09:15:31 +0200

    Exam

---
+++ C# Fundamentals/8. Mid Exam Preparation/Mid Exam - 5 July 2020/03. Numbers/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03._Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+            double average = numbers.Average();
+
+            numbers = numbers.Where(x => x > average).OrderByDescending(x => x).Take(5).ToList();
+
+            if (numbers.Count == 0)
+            {
+                Console.WriteLine("No");
+            }
+            else
+            {
+                Console.WriteLine(string.Join(' ', numbers));
+            }
+
+        }
+    }
+}
