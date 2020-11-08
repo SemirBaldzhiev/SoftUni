e208326e8db7cb4abac4d485312c60ff15859082
Semir Baldzhiev <semir.baldzhiev@gmail.com>
2020-11-08 10:32:05 +0200

    Exam

---
+++ C# Fundamentals/9. Mid Exam/9. Mid Exam/Problem1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Problem1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int biscuitsPerDayPerWorker = int.Parse(Console.ReadLine());
+            int workers = int.Parse(Console.ReadLine());
+            int biscuitsForMonth = int.Parse(Console.ReadLine());
+
+            int biscuitsForDay = biscuitsPerDayPerWorker * workers;
+            double biscuitsForEachThidDay = 10 * (biscuitsForDay - Math.Floor((biscuitsForDay * 0.75)));
+
+            double totalBiscuits = 30 * biscuitsForDay - biscuitsForEachThidDay;
+
+            double percentage = (Math.Abs(totalBiscuits - biscuitsForMonth)) / (double)biscuitsForMonth * 100;
+           
+            Console.WriteLine($"You have produced {totalBiscuits} biscuits for the past month.");
+
+            if (totalBiscuits > biscuitsForMonth)
+            {
+                Console.WriteLine($"You produce {percentage:f2} percent more biscuits.");
+            }
+            else
+            {
+                Console.WriteLine($"You produce {percentage:f2} percent less biscuits.");
+            }
+
+
+            
+        }
+    }
+}
