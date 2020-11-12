571195daef480887df00b22a2aa81207b068f64b
Semir Baldzhiev <semir.baldzhiev@gmail.com>
2020-11-12 12:32:41 +0200

    Exercise

---
+++ C# Fundamentals/10. Associative Arrays/02. MinerTask/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _02._MinerTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Dictionary<string, int> dictionary = new Dictionary<string, int>(); 
+
+            while (true)
+            {
+                string resource = Console.ReadLine();
+
+                if (resource == "stop")
+                {
+                    break;
+                }
+
+                int quantity = int.Parse(Console.ReadLine());
+
+                if (dictionary.ContainsKey(resource))
+                {
+                    dictionary[resource] += quantity;
+                }
+                else
+                {
+                    dictionary.Add(resource, quantity);
+                }
+
+            }
+
+
+            foreach (var resource in dictionary)
+            {
+                Console.WriteLine($"{resource.Key} -> {resource.Value}");
+            }
+
+        }
+    }
+}
