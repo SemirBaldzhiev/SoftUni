b00d8911eaae3755b18070b532fd2205f93efde9
Semir Baldzhiev <semir.baldzhiev@gmail.com>
2020-11-12 12:09:59 +0200

    Exercise

---
+++ C# Fundamentals/10. Associative Arrays/01. CountCharsInString/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _01._CountCharsInString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            Dictionary<char, int> dictionary = new Dictionary<char, int>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+
+                if (input[i] != ' ')
+                {
+                    if (dictionary.ContainsKey(input[i]))
+                    {
+                        dictionary[input[i]]++;
+                    }
+                    else
+                    {
+                        dictionary.Add(input[i], 1);
+                    }
+                }
+                
+            }
+
+            foreach (var item in dictionary)
+            {
+                Console.WriteLine($"{item.Key} -> {item.Value}");
+            }
+        }
+    }
+}
