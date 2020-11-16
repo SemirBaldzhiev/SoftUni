93b3fcd51ccaa29e36e9f0ec392beaae1e3f64e4
Semir Baldzhiev <semir.baldzhiev@gmail.com>
2020-11-16 14:50:10 +0200

    Exercise

---
+++ C# Fundamentals/10. Associative Arrays/06. Courses/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _06._Courses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, List<string>> coursesInfo = new Dictionary<string, List<string>>();
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+
+                if (input == "end")
+                {
+                    break;
+                }
+
+                string[] commands = input.Split(" : ").ToArray();
+
+                string course = commands[0];
+                string student = commands[1];
+
+                if (coursesInfo.ContainsKey(course))
+                {
+                    coursesInfo[course].Add(student);
+                }
+                else
+                {
+                    coursesInfo.Add(course, new List<string>() { student });
+                }
+            }
+
+            foreach (var course in coursesInfo.OrderByDescending(x => x.Value.Count))
+            {
+                Console.WriteLine($"{course.Key}: {course.Value.Count}");
+
+                foreach (var student in course.Value.OrderBy(x => x))
+                {
+                    Console.WriteLine($"-- {student}");
+                }
+            }
+        }
+    }
+}
