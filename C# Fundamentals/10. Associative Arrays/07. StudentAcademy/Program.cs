6b50a7969ddd05e22452488a53684926bc4ffc27
Semir Baldzhiev <semir.baldzhiev@gmail.com>
2020-11-16 15:32:50 +0200

    Exercise

---
+++ C# Fundamentals/10. Associative Arrays/07. StudentAcademy/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _07._StudentAcademy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Dictionary<string, List<double>> students = new Dictionary<string, List<double>>();
+
+            Dictionary<string, double> filteredStudents = new Dictionary<string, double>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string name = Console.ReadLine();
+                double grade = double.Parse(Console.ReadLine());
+
+                if (students.ContainsKey(name))
+                {
+                    students[name].Add(grade);
+                }
+                else
+                {
+                    students.Add(name, new List<double>() { grade });
+                }
+
+            }
+
+
+            foreach (var item in students)
+            {
+                double average = item.Value.Average();
+
+                if (average >= 4.50)
+                {
+                    filteredStudents.Add(item.Key, average);
+                }
+            }
+
+            
+            foreach (var student in filteredStudents.OrderByDescending(x => x.Value))
+            {
+                Console.WriteLine($"{student.Key} -> {student.Value:f2}");
+            }
+        }
+    }
+}
