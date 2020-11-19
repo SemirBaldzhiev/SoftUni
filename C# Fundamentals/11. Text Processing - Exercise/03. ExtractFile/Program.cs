5fe2e41b064aad65488226474383fd3db7cee90d
Semir Baldzhiev <semir.baldzhiev@gmail.com>
2020-11-19 19:49:48 +0200

    Exercise

---
+++ C# Fundamentals/11. Text Processing - Exercise/03. ExtractFile/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _03._ExtractFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = Console.ReadLine();
+
+            int index = filePath.LastIndexOf("\\");
+
+            string[] file = filePath.Substring(index + 1).Split('.');
+
+            string fileName = file[0];
+            string extension = file[1];
+
+            Console.WriteLine($"File name: {fileName}");
+            Console.WriteLine($"File extension: {extension}");
+        }
+    }
+}
