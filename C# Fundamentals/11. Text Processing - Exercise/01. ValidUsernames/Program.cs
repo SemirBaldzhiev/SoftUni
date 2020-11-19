167ccd8805184f23a443e8385dd6b0ccb96235bc
Semir Baldzhiev <semir.baldzhiev@gmail.com>
2020-11-19 19:04:36 +0200

    Exercise

---
+++ C# Fundamentals/11. Text Processing - Exercise/01. ValidUsernames/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Linq;
+
+namespace _01._ValidUsernames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] usernames = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+
+            bool isValid = false;
+
+
+            foreach (var name in usernames)
+            {
+
+                char[] nameToChar;
+
+               
+
+                if (name.Length >= 3 && name.Length <= 16)
+                {
+                    isValid = true;
+                }
+                else
+                {
+                    isValid = false;
+                    continue;
+                }
+
+                nameToChar = name.ToCharArray();
+
+                foreach (var ch in nameToChar)
+                {
+                    if (char.IsLetterOrDigit(ch))
+                    {
+                        isValid = true;
+                    }
+                    else
+                    {
+                        isValid = false;
+                        break;
+                    }
+                }
+
+                if (name.Contains('-') || name.Contains('_'))
+                {
+                    isValid = true;
+                }
+
+                if (isValid)
+                {
+                    Console.WriteLine(name);
+                }
+            }
+
+        }
+    }
+}
