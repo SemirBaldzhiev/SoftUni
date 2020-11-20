52bac7d78275a44a295313ec382d2b79f06a147a
Semir Baldzhiev <semir.baldzhiev@gmail.com>
2020-11-20 16:29:34 +0200

    Exercise

---
+++ C# Fundamentals/11. Text Processing - Exercise/06. ReplaceRepeatingChars/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace _06._ReplaceRepeatingChars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < input.Length - 1; i++)
+            {
+
+                if (input[i] != input[i + 1])
+                {
+                    sb.Append(input[i]);
+                }
+
+                if (i == input.Length - 2)
+                {
+                    if (input[i] == input[i + 1])
+                    {
+                        sb.Append(input[i]);
+                    }
+                    else
+                    {
+                        sb.Append(input[i + 1]);
+                    }
+                }
+            }
+
+            Console.WriteLine(sb);
+            
+        }
+    }
+}
