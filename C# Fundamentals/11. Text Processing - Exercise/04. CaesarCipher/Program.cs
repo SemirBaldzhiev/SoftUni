539d6fbd9bccf56c79323edb7a885f9e1ba34302
Semir Baldzhiev <semir.baldzhiev@gmail.com>
2020-11-20 16:28:21 +0200

    Exercise

---
+++ C# Fundamentals/11. Text Processing - Exercise/04. CaesarCipher/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text;
+
+namespace _04._CaesarCipher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var ch in text)
+            {
+                sb.Append((char)(ch + 3));
+            }
+
+            Console.WriteLine(sb);
+        }
+    }
+}
