3a1b7bf9fc326f174f0132300a4abf3e3f95131e
Semir Baldzhiev <semir.baldzhiev@gmail.com>
2020-11-25 12:18:52 +0200

    Exercise

---
+++ C# Fundamentals/11. Text Processing - Exercise/07. StringExplosion/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text;
+using System.Linq;
+
+namespace _07._StringExplosion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int strength = 0;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == '>')
+                {
+                    strength += int.Parse(input[i + 1].ToString());
+                    continue;
+                }
+
+                if (strength > 0)
+                {
+                    input = input.Remove(i, 1);
+                    strength--;
+                    i--;
+                }
+            }
+
+            Console.WriteLine(input);
+        }
+    }
+}
