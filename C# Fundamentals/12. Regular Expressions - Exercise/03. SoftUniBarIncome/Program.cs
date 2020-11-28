7f7a941cbdf26b283b6a2592173bfc3af3aa470c
Semir Baldzhiev <semir.baldzhiev@gmail.com>
2020-11-28 18:35:15 +0200

    Exercise

---
+++ C# Fundamentals/12. Regular Expressions - Exercise/03. SoftUniBarIncome/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _03._SoftUniBarIncome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"%([A-Z][a-z]+)%[^|$%.]*<(\w+)>[^|$%.]*\|(\d+)\|[^|$%.]*?(\d+\.?\d+)\$";
+            
+            string input = Console.ReadLine();
+            Regex regex = new Regex(pattern);
+            double totalIncome = 0.0;
+
+            while (input != "end of shift")
+            {
+                var match = regex.Match(input);
+
+                if (match.Success)
+                {
+                    string customer = match.Groups[1].Value;
+                    string product = match.Groups[2].Value;
+                    int count = int.Parse(match.Groups[3].Value);
+                    double price = double.Parse(match.Groups[4].Value);
+
+                    double totalPrice = count * price;
+
+                    Console.WriteLine($"{customer}: {product} - {totalPrice:f2}");
+
+                    totalIncome += totalPrice;
+                }
+
+
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine($"Total income: {totalIncome:f2}");
+        }
+    }
+}
