a73c51f0aa2d987bb05803fd507c4c2ea33de5d2
Semir Baldzhiev <semir.baldzhiev@gmail.com>
2020-12-02 16:31:28 +0200

    Exercise

---
+++ C# Fundamentals/12. Regular Expressions - Exercise/06. ExtractEmails/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _06._ExtractEmails
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            string pattern = @"(?<=\s)([A-Za-z0-9]+[\.\-_]?[A-Za-z0-9]+)@([A-Za-z]+[\-]?[A-Za-z]+)(\.([A-Za-z]+)){1,}";
+
+            Regex regex = new Regex(pattern);
+
+            var matches = regex.Matches(text);
+
+            foreach (Match match in matches)
+            {
+                Console.WriteLine(match.Value);
+            }
+
+        }
+    }
+}
