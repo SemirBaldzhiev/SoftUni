6908f50e9f650a662de286102f3ec4919c6c9f01
Semir Baldzhiev <semir.baldzhiev@gmail.com>
2021-01-16 16:46:06 +0200

    Added matching brackets task from lab

---
+++ C#-Advanced/01.Stacks-And-Queues-Lab/MatchingBrackets/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MatchingBrackets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            Stack<int> bracketIndexes = new Stack<int>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == '(')
+                {
+                    bracketIndexes.Push(i);
+                }
+
+                if (input[i] == ')')
+                {
+                    int index = bracketIndexes.Pop();
+                    string expression = input.Substring(index, i - index + 1);
+                    Console.WriteLine(expression);
+                }
+            }
+        }
+    }
+}
