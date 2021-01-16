56355d515e2aade4464eb54780ea6a58c9e9979e
Semir Baldzhiev <semir.baldzhiev@gmail.com>
2021-01-16 19:06:11 +0200

    Added traffic jam task from lab

---
+++ C#-Advanced/01.Stacks-And-Queues-Lab/TrafficJam/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TrafficJam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Queue<string> cars = new Queue<string>();
+            int count = 0;
+
+            string input = Console.ReadLine();
+
+            while (input != "end")
+            {
+                if (input == "green")
+                {
+                    if (n > cars.Count)
+                    {
+                        n = cars.Count;
+                    }
+
+                    for (int i = 0; i < n; i++)
+                    {
+                        Console.WriteLine($"{cars.Dequeue()} passed!");
+                        count++;
+                    }
+                }
+                else
+                {
+                    cars.Enqueue(input);
+                }
+
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine($"{count} cars passed the crossroads.");
+        }
+    }
+}
