b0aa2ad37ed31ae5a634abb8c877756af21c000e
Semir Baldzhiev <semir.baldzhiev@gmail.com>
2021-01-16 17:11:59 +0200

    Added even numbers task from lab

---
+++ C#-Advanced/01.Stacks-And-Queues-Lab/PintEvenNumbers/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PrintEvenNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] inputData = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            Queue<int> numbers = new Queue<int>();
+
+            
+
+            for (int i = 0; i < inputData.Length; i++)
+            {
+                if (inputData[i] % 2 == 0)
+                {
+                    numbers.Enqueue(inputData[i]);
+                }
+
+            }
+
+            Console.WriteLine(string.Join(", ", numbers));
+        }
+    }
+}
