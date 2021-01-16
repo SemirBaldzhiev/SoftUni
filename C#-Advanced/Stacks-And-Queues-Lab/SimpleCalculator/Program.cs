bfa913c1cbe02cce0319a08ccbd75805b3f3b020
Semir Baldzhiev <semir.baldzhiev@gmail.com>
2021-01-16 15:58:19 +0200

    Adeed simple calculator from stack and queues

---
+++ C#-Advanced/Stacks-And-Queues-Lab/SimpleCalculator/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SimpleCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split();
+
+            Stack<string> expression = new Stack<string>();
+
+            for (int i = input.Length - 1; i >= 0; i--)
+            {
+                expression.Push(input[i]);
+            }
+
+            while (expression.Count > 1)
+            {
+                int firstNumber = int.Parse(expression.Pop());
+                string sign = expression.Pop();
+                int secondNumber = int.Parse(expression.Pop());
+
+                if (sign == "+")
+                {
+                    expression.Push((firstNumber + secondNumber).ToString());
+                }
+                if (sign == "-")
+                {
+                    expression.Push((firstNumber - secondNumber).ToString());
+                }
+
+            }
+
+            Console.WriteLine(expression.Pop());
+        }
+    }
+}
