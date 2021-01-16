75377d9e31764591dad88539bcd83a6d6e0660eb
Semir Baldzhiev <semir.baldzhiev@gmail.com>
2021-01-16 15:02:40 +0200

    Added reverse string task from Stacks and Queues

---
+++ C#-Advanced/Stacks-And-Queues-Lab/ReverseStrings/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ReverseStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Stack<char> reverseInput = new Stack<char>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                reverseInput.Push(input[i]);
+            }
+
+            foreach (var ch in reverseInput)
+            {
+                Console.Write(ch);
+            }
+            Console.WriteLine();
+        }
+    }
+}
