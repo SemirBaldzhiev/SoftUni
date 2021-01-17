030cd4279eda807098e03d3349f10f3b6770e264
Semir Baldzhiev <semir.baldzhiev@gmail.com>
2021-01-17 16:32:50 +0200

    Added task 4 from exercises

---
+++ C#-Advanced/02.Stacks-And-Queues-Exercises/FastFood/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FastFood
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //499
+            //57 45 62 70 33 90 88 76
+
+            int foodQuantity = int.Parse(Console.ReadLine());
+            int[] ordersData = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            Queue<int> orders = new Queue<int>(ordersData);
+            bool isCompleted = true;
+            int count = orders.Count();
+
+            int maxOrder = orders.Max();
+
+            for (int i = 0; i < count; i++)
+            {
+                if (foodQuantity - orders.Peek() >= 0)
+                {
+                    foodQuantity -= orders.Dequeue();
+                }
+                else
+                {
+                    isCompleted = false;
+                    break;
+                }
+            }
+
+            Console.WriteLine(maxOrder);
+
+            if (isCompleted)
+            {
+                Console.WriteLine("Orders complete");
+            }
+            else
+            {
+                Console.WriteLine($"Orders left: {string.Join(" ", orders)}");
+            }
+        }
+    }
+}
