12415ccc254baacf7892012a2f04b033a8a9d73d
Semir Baldzhiev <semir.baldzhiev@gmail.com>
2021-01-17 14:20:19 +0200

    Added basic queue operations task from exercises

---
+++ C#-Advanced/02.Stacks-And-Queues-Exercises/BasicQueueOperations/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BasicQueueOperations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] inputData = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int[] inputNumbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int n = inputData[0];
+            int s = inputData[1];
+            int x = inputData[2];
+
+            Queue<int> numbers = new Queue<int>();
+
+            for (int i = 0; i < n; i++)
+            {
+                numbers.Enqueue(inputNumbers[i]);
+            }
+
+            for (int i = 0; i < s; i++)
+            {
+                numbers.Dequeue();
+            }
+
+
+            if (numbers.Count == 0)
+            {
+                Console.WriteLine(0);
+            }
+            else
+            {
+                if (numbers.Contains(x))
+                {
+                    Console.WriteLine("true");
+                }
+                else
+                {
+                    Console.WriteLine(numbers.Min());
+                }
+            }
+
+        }
+    }
+}
