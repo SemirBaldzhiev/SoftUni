4674b7d1eaf8890ddd9df87a62052f3bf9b881ca
Semir Baldzhiev <semir.baldzhiev@gmail.com>
2021-01-18 13:01:42 +0200

    Added task 8 from exercises

---
+++ C#-Advanced/02.Stacks-And-Queues-Exercises/BalancedParenthesis/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BalancedParenthesis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //{[(}]}
+            string input = Console.ReadLine();
+
+            Stack<char> brackets = new Stack<char>();
+            bool isBalanced = true;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == '[' || input[i] == '{'  || input[i] == '(')
+                {
+                    brackets.Push(input[i]);
+                }
+                else
+                {
+                    if (brackets.Count == 0)
+                    {
+                        isBalanced = false;
+                        break;
+                    }
+
+                    bool isfirstBracketValid = input[i] == ')' && brackets.Pop() == '(';//false
+                    bool isSecondBracketValid = input[i] == '}' && brackets.Pop() == '{'; //false
+                    bool isThirdBracketValid = input[i] == ']' && brackets.Pop() == '[';//false
+
+                    if (!isfirstBracketValid && !isSecondBracketValid && !isThirdBracketValid)
+                    {
+                        isBalanced = false;
+                        break;
+                    }
+                }
+                
+            }
+
+            if (isBalanced)
+            {
+                Console.WriteLine("YES");
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
+        }
+    }
+}
