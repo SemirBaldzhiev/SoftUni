3e963a947dee136290100607beb4033b99ffee63
Semir Baldzhiev <semir.baldzhiev@gmail.com>
2021-01-23 10:29:53 +0200

    Added task 4 from sets and dictionaries exercises

---
+++ C#-Advanced/Sets-And-Dictionaries-Advanced-Exercises/EvenTimes/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EvenTimes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Dictionary<int, int> dictionary = new Dictionary<int, int>();
+
+            for (int i = 0; i < n; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+
+                if (!dictionary.ContainsKey(num))
+                {
+                    dictionary.Add(num, 0);
+                }
+
+                dictionary[num]++;
+            }
+
+            int evenTimesNumber = dictionary.Where(x => x.Value % 2 == 0).Select(x => x.Key).FirstOrDefault();
+
+            Console.WriteLine(evenTimesNumber);
+        }
+    }
+}
