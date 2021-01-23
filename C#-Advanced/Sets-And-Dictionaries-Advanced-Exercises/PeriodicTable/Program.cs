99288a8165018af88b26d5d2a853787dc684d14d
Semir Baldzhiev <semir.baldzhiev@gmail.com>
2021-01-23 10:14:29 +0200

    Added task 3 from sets and dictionaries exercises

---
+++ C#-Advanced/Sets-And-Dictionaries-Advanced-Exercises/PeriodicTable/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PeriodicTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            HashSet<string> chemicalElements = new HashSet<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] elements = Console.ReadLine().Split().ToArray();
+
+                for (int j = 0; j < elements.Length; j++)
+                {
+                    chemicalElements.Add(elements[j]);
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", chemicalElements.OrderBy(x => x)));
+        }
+    }
+}
