c38c2a21fc4b10b5dd7537173103659d4d9f2809
Semir Baldzhiev <semir.baldzhiev@gmail.com>
2021-01-23 10:37:38 +0200

    Added task 5 from sets and dictionaries exerises

---
+++ C#-Advanced/Sets-And-Dictionaries-Advanced-Exercises/CountSymbols/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CountSymbols
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+
+            Dictionary<char, int> symbolsCount = new Dictionary<char, int>();
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (!symbolsCount.ContainsKey(text[i]))
+                {
+                    symbolsCount.Add(text[i], 0);
+                }
+
+                symbolsCount[text[i]]++;
+            }
+
+            foreach (var symbol in symbolsCount.OrderBy(s => s.Key))
+            {
+                Console.WriteLine($"{symbol.Key}: {symbol.Value} time/s");
+            }
+
+        }
+    }
+}
