fbf49d09971ed580b61556f47a73b1bfd53b8be3
Semir Baldzhiev <semir.baldzhiev@gmail.com>
2021-01-23 10:08:04 +0200

    Added task 2 from sets and dictionaries exercises

---
+++ C#-Advanced/Sets-And-Dictionaries-Advanced-Exercises/SetsOfElements/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SetsOfElements
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int n = input[0];
+            int m = input[1];
+
+            HashSet<int> firstSet = new HashSet<int>();
+            HashSet<int> secondSet = new HashSet<int>();
+
+
+            for (int i = 0; i < n; i++)
+            {
+
+                int num = int.Parse(Console.ReadLine());
+                firstSet.Add(num);
+            }
+
+            for (int i = 0; i < m; i++)
+            {
+                int secondNum = int.Parse(Console.ReadLine());
+                secondSet.Add(secondNum);
+            }
+
+            Console.WriteLine(string.Join(" ", firstSet.Intersect(secondSet)));
+        }
+    }
+}
