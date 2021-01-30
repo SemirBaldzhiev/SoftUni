dec25a94a94f35f2ca20fbef5cd755549b66ea83
Semir Baldzhiev <semir.baldzhiev@gmail.com>
2021-01-30 14:54:23 +0200

    Added task 3 from exercises

---
+++ C#-Advanced/06.Functional-Programming-Exercise/02.KnightsOfHonor/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02.KnightsOfHonor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split().ToArray();
+
+            List<string> modifiedNames = input.Select(x => $"Sir {x}").ToList();
+
+            Action<List<string>> printNames = names => Console.WriteLine(string.Join(Environment.NewLine, names));
+
+            printNames(modifiedNames);
+        }
+    }
+}
