11cd9d21e05c8bd480e29f00fa080a8066461e08
Semir Baldzhiev <semir.baldzhiev@gmail.com>
2021-02-02 12:22:17 +0200

    Added task 9 from exercises

---
+++ C#-Advanced/06.Functional-Programming-Exercise/09.ListOfPredicates/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _09.ListOfPredicates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int end = int.Parse(Console.ReadLine());
+            List<int> dividers = Console.ReadLine().Split().Select(int.Parse).ToList();
+            List<int> numbers = new List<int>();
+
+            Func<int, int, bool> predicate = (num, div) => num % div == 0;
+
+            for (int i = 1; i <= end; i++)
+            {
+                if (dividers.All(d => predicate(i, d)))
+                {
+                    numbers.Add(i);
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+    }
+}
