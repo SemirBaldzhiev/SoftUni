3a99e5285b3a6351f69f36c56b59a968b0aec0e2
Semir Baldzhiev <semir.baldzhiev@gmail.com>
2021-02-03 10:09:48 +0200

    Added task 12 from exercises

---
+++ C#-Advanced/06.Functional-Programming-Exercise/12.TriFunction/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Linq;
+
+namespace _12.TriFunction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            string[] names = Console.ReadLine().Split().ToArray();
+
+            Func<string, int, bool> func = (name, num) =>
+            {
+                bool isLarger = false;
+
+                int sum = 0;
+                for (int i = 0; i < name.Length; i++)
+                {
+                    sum += (int)name[i];
+                }
+
+                if (sum >= num)
+                {
+                    isLarger = true;
+                }
+
+                return isLarger;
+            };
+
+            string largestSumName = FindLargestCharSum(names, n, func);
+
+            Console.WriteLine(largestSumName);
+
+        }
+
+        public static string FindLargestCharSum(string[] names, int n, Func<string, int, bool> func)
+        {
+            string result = string.Empty;
+
+            foreach (var name in names)
+            {
+                if (func(name, n))
+                {
+                    result = name;
+                    break;
+                }
+            }
+
+            return result;
+        }
+    }
+}
