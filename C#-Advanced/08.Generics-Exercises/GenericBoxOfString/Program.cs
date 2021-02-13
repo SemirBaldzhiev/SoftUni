bed618d2a816c2f7f7c0b568d66c05f035900afc
Semir Baldzhiev <semir.baldzhiev@gmail.com>
2021-02-13 13:15:39 +0200

    Added task 1 from generic exercises

---
+++ C#-Advanced/08.Generics-Exercises/GenericBoxOfString/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GenericBoxOfString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            List<Box<string>> boxes = new List<Box<string>>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string input = Console.ReadLine();
+                Box<string> box = new Box<string>(input);
+                boxes.Add(box);
+            }
+
+            foreach (var box in boxes)
+            {
+                Console.WriteLine(box);
+            }
+        }
+    }
+}
