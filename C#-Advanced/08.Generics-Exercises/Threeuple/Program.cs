c85cbcd0590903a703afd23d5672220a64afe95f
Semir Baldzhiev <semir.baldzhiev@gmail.com>
2021-02-13 15:18:48 +0200

    Added task 8 from generic exercises

---
+++ C#-Advanced/08.Generics-Exercises/Threeuple/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+
+namespace Threeuple
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] firstLine = Console.ReadLine().Split().ToArray();
+
+            string fullName = firstLine[0] + " " + firstLine[1];
+
+            string city = string.Join(" ", firstLine.Skip(3));
+
+            Threeuple<string, string, string> firstThreeuple =
+                new Threeuple<string, string, string>(fullName, firstLine[2], city);
+
+            string[] secondLine = Console.ReadLine().Split().ToArray();
+
+            bool isDrunk = secondLine[2] == "drunk";
+
+            Threeuple<string, int, bool> seconThreeuple =
+                new Threeuple<string, int, bool>(secondLine[0], int.Parse(secondLine[1]), isDrunk);
+
+            string[] thirdLine = Console.ReadLine().Split().ToArray();
+
+            Threeuple<string, double, string> thirdThreeuple =
+                new Threeuple<string, double, string>(thirdLine[0], double.Parse(thirdLine[1]), thirdLine[2]);
+
+            Console.WriteLine($"{firstThreeuple.Item1} -> {firstThreeuple.Item2} -> {firstThreeuple.Item3}");
+            Console.WriteLine($"{seconThreeuple.Item1} -> {seconThreeuple.Item2} -> {seconThreeuple.Item3}");
+            Console.WriteLine($"{thirdThreeuple.Item1} -> {thirdThreeuple.Item2} -> {thirdThreeuple.Item3}");
+        }
+    }
+}
---
+++ C#-Advanced/08.Generics-Exercises/Threeuple/Threeuple.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Threeuple
+{
+    public class Threeuple<T1, T2, T3>
+    {
+        private T1 item1;
+        private T2 item2;
+        private T3 item3;
+
+        public Threeuple(T1 item1, T2 item2, T3 item3)
+        {
+            Item1 = item1;
+            Item2 = item2;
+            Item3 = item3;
+        }
+
+        public T1 Item1
+        {
+            get => item1;
+            set => item1 = value;
+        }
+        public T2 Item2
+        {
+            get => item2;
+            set => item2 = value;
+        }
+        public T3 Item3
+        {
+            get => item3;
+            set => item3 = value;
+        }
+
+
+
+    }
+}
