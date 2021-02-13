77719337b5ec0194d9e271774d611a76d9d677aa
Semir Baldzhiev <semir.baldzhiev@gmail.com>
2021-02-13 14:49:12 +0200

    Added task 5 froom generic exercises

---
+++ C#-Advanced/08.Generics-Exercises/GenericCountMethodStrings/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GenericCountMethodStrings
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            List<Box<string>> boxes = new List<Box<string>>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string input = Console.ReadLine();
+                Box<string> box = new Box<string>(input);
+                boxes.Add(box);
+            }
+
+            string criteria = Console.ReadLine();
+
+            Box<string> comparableBox = new Box<string>(criteria);
+
+            int count = GetGreaterThanCount(boxes, comparableBox);
+
+            Console.WriteLine(count);
+        }
+
+        private static int GetGreaterThanCount<T>(List<Box<T>> boxes, Box<T> box) 
+            where T: IComparable
+        {
+            int count = 0;
+
+            foreach (var item in boxes)
+            {
+                int compare = item.Value.CompareTo(box.Value);
+
+                if (compare > 0)
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+    }
+}
