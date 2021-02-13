72cb60e786d7f4a2aaea89d0becab5f3026ec71a
Semir Baldzhiev <semir.baldzhiev@gmail.com>
2021-02-13 13:16:39 +0200

    Added solutions folder for generic exercises

---
+++ C#-Advanced/08.Generics-Exercises/GenericBoxOfIntegers/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GenericBoxOfIntegers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            List<Box<int>> boxes = new List<Box<int>>();
+
+            for (int i = 0; i < n; i++)
+            {
+                int input = int.Parse(Console.ReadLine());
+                Box<int> box = new Box<int>(input);
+                boxes.Add(box);
+            }
+
+            foreach (var box in boxes)
+            {
+                Console.WriteLine(box);
+            }
+        }
+    }
+}
