e1e812df793f56c906170ff0e6a3b4da259a16c7
Semir Baldzhiev <semir.baldzhiev@gmail.com>
2021-02-14 19:06:16 +0200

    Added task 3 from exam preparation

---
+++ C#-Advanced/10.Exam-Preparation/VetClinic/Clinic.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VetClinic
+{
+    public class Clinic
+    {
+        private List<Pet> data;
+
+        public Clinic(int capacity)
+        {
+            Capacity = capacity;
+            data = new List<Pet>();
+        }
+        public int Capacity { get; set; }
+        public int Count { get; private set; }
+
+        public void Add(Pet pet)
+        {
+            if (Capacity > data.Count)
+            {
+                data.Add(pet);
+                Count++;
+            }
+        }
+
+        public bool Remove(string name)
+        {
+            Pet pet = data.Find(p => p.Name == name);
+            if (pet != null)
+            {
+                data.Remove(pet);
+                Count--;
+                return true;
+            }
+
+            return false;
+        }
+
+        public Pet GetPet(string name, string owner)
+        {
+            Pet pet = data.Where(x => x.Name == name && x.Owner == owner).FirstOrDefault();
+
+            return pet;
+        }
+
+        public Pet GetOldestPet()
+        {
+            Pet oldestPet = data.OrderByDescending(x => x.Age).FirstOrDefault();
+
+            return oldestPet;
+        }
+
+        public string GetStatistics()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("The clinic has the following patients:");
+
+            data.ForEach(p => sb.AppendLine($"Pet {p.Name} with owner: {p.Owner}"));
+
+            return sb.ToString().Trim();
+        }
+
+
+    }
+}
