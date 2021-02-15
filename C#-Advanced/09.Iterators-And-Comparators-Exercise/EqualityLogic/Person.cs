e394c563896d96d7cdf1e0d0eb5e795f48e62d46
Semir Baldzhiev <semir.baldzhiev@gmail.com>
2021-02-15 23:10:25 +0200

    Added task 6 from exercise

---
+++ C#-Advanced/09.Iterators-And-Comparators-Exercise/EqualityLogic/Person.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EqualityLogic
+{
+    public class Person : IComparable<Person>
+    {
+
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public int CompareTo(Person other)
+        {
+            if (Name.CompareTo(other.Name) != 0)
+            {
+                return Name.CompareTo(other.Name);
+            }
+
+            if (Age.CompareTo(other.Age) != 0)
+            {
+                return Age.CompareTo(other.Age);
+            }
+
+            return 0;
+        }
+
+        public override int GetHashCode()
+        {
+            int nameHash = Name.GetHashCode();
+            int ageHash = Age.GetHashCode();
+
+            return nameHash + ageHash;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return this.GetHashCode() == obj.GetHashCode();
+        }
+    }
+}
---
+++ C#-Advanced/09.Iterators-And-Comparators-Exercise/EqualityLogic/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EqualityLogic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            SortedSet<Person> sortedSet = new SortedSet<Person>();
+            HashSet<Person> hashSet = new HashSet<Person>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] personData = Console.ReadLine().Split().ToArray();
+
+                Person person = new Person(personData[0], int.Parse(personData[1]));
+
+                sortedSet.Add(person);
+                hashSet.Add(person);
+            }
+
+            Console.WriteLine(sortedSet.Count);
+            Console.WriteLine(hashSet.Count);
+        }
+    }
+}
