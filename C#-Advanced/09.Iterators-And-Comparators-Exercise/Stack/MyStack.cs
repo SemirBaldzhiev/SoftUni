9665a4653a5a4b0aef03d37df454dbb1272b5d96
Semir Baldzhiev <semir.baldzhiev@gmail.com>
2021-02-15 22:39:15 +0200

    Added task 3 from iterators and comparators

---
+++ C#-Advanced/09.Iterators-And-Comparators-Exercise/Stack/MyStack.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Stack
+{
+    public class MyStack<T> : IEnumerable<T>
+    {
+        private List<T> items;
+
+        public MyStack()
+        {
+            items = new List<T>();
+        }
+
+        public MyStack(List<T> items)
+        {
+            this.items = items;
+        }
+
+        public void Push(T item)
+        {
+            items.Add(item);
+        }
+
+        public T Pop()
+        {
+            int index = items.Count - 1;
+
+            if (index < 0)
+            {
+                throw new InvalidOperationException("No elements");
+            }
+            T item = items[index];
+            items.Remove(item);
+
+            return item;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = items.Count - 1; i >= 0; i--)
+            {
+                yield return items[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
--- C#-Advanced/09.Iterators-And-Comparators-Exercise/Stack/Program.cs
+++ C#-Advanced/09.Iterators-And-Comparators-Exercise/Stack/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace Stack
 {
@@ -6,7 +7,45 @@ namespace Stack
     {
         static void Main(string[] args)
         {
-            
+            MyStack<int> stack = new MyStack<int>();
+
+            string input = Console.ReadLine();
+
+            while (input != "END")
+            {
+                string[] commands = input.Split(new string[] { " ", ", "}, StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+                string command = commands[0];
+
+                if (command == "Push")
+                {
+                    for (int i = 1; i < commands.Length; i++)
+                    {
+                        stack.Push(int.Parse(commands[i]));
+                    }
+                }
+                else if (command == "Pop")
+                {
+                    try
+                    {
+                        stack.Pop();
+                    }
+                    catch (InvalidOperationException e)
+                    {
+                        Console.WriteLine(e.Message);
+                    }
+                    
+                }
+
+                input = Console.ReadLine();
+            }
+
+            foreach (var item in stack)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine(string.Join(Environment.NewLine, stack));
         }
     }
 }
