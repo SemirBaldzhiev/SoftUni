b45d517cce7de16613eae500ae1322a5dba8ca6c
Semir Baldzhiev <semir.baldzhiev@gmail.com>
2021-02-15 23:23:45 +0200

    Added task 5 from exercises

---
+++ C#-Advanced/09.Iterators-And-Comparators-Exercise/ComparingObjects/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ComparingObjects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            List<Person> people = new List<Person>();
+
+            while (input != "END")
+            {
+                string[] personData = input.Split().ToArray();
+
+                Person person = new Person(personData[0], int.Parse(personData[1]), personData[2]);
+                people.Add(person);
+
+                input = Console.ReadLine();
+            }
+
+            int index = int.Parse(Console.ReadLine());
+
+            var personToCompare = people[index - 1];
+
+            int equalCount = 0;
+            int notEqualCount = 0;
+
+            foreach (var person in people)
+            {
+                if (personToCompare.CompareTo(person) == 0)
+                {
+                    equalCount++;
+                }
+                else
+                {
+                    notEqualCount++;
+                }
+            }
+
+            if (equalCount > 1)
+            {
+                Console.WriteLine($"{equalCount} {notEqualCount} {people.Count}");
+            }
+            else
+            {
+                Console.WriteLine("No matches");
+            }
+        }
+    }
+}
---
+++ C#-Advanced/10.Exam-Preparation/Selling/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Selling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            char[,] matrix = new char[n, n];
+
+            int sellerRow = -1;
+            int sellerCol = -1;
+
+            for (int row = 0; row < n; row++)
+            {
+                string currentRow = Console.ReadLine();
+
+                for (int col = 0; col < n; col++)
+                {
+                    matrix[row, col] = currentRow[col];
+
+                    if (matrix[row, col] == 'S')
+                    {
+                        sellerRow = row;
+                        sellerCol = col;
+                    }
+
+                    if (matrix[row, col] == 'O')
+                    {
+
+                    }
+                }
+            }
+
+
+        }
+    }
+}
