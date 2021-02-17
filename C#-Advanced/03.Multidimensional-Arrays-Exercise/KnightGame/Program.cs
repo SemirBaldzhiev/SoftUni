fe6352190d29733b495ded35e15fceaf454fc854
Semir Baldzhiev <semir.baldzhiev@gmail.com>
2021-02-17 22:53:33 +0200

    Added task 7 form exercises

---
+++ C#-Advanced/03.Multidimensional-Arrays-Exercise/KnightGame/Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+
+namespace KnightGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            char[,] board = new char[n, n];
+
+            for (int row = 0; row < n; row++)
+            {
+                string rowData = Console.ReadLine();
+
+                for (int col = 0; col < n; col++)
+                {
+                    board[row, col] = rowData[col];
+                }
+            }
+
+            int removedKnightsCount = 0;
+
+            while (true)
+            {
+                int maxAttackedKnightsCount = 0;
+                int knightRow = -1;
+                int knightCol = -1;
+
+                for (int row = 0; row < n; row++)
+                {
+                    for (int col = 0; col < n; col++)
+                    {
+                        char symbol = board[row, col];
+
+                        if (symbol != 'K')
+                        {
+                            continue;
+                        }
+
+                        int count = GetCountOfAttackedKnights(board, row, col);
+
+                        if (count > maxAttackedKnightsCount)
+                        {
+                            maxAttackedKnightsCount = count;
+                            knightRow = row;
+                            knightCol = col;
+                        }
+                    }
+                }
+
+                if (maxAttackedKnightsCount == 0)
+                {
+                    break;
+                }
+
+                board[knightRow, knightCol] = '0';
+                removedKnightsCount++;
+            }
+
+            Console.WriteLine(removedKnightsCount);
+        }
+
+        private static int GetCountOfAttackedKnights(char[,] board, int row, int col)
+        {
+            int count = 0;
+
+            if (ContainsKnight(board, row - 2, col - 1))
+            {
+                count++;
+            }
+
+            if (ContainsKnight(board, row - 2, col + 1))
+            {
+                count++;
+            }
+
+            if (ContainsKnight(board, row - 1, col - 2))
+            {
+                count++;
+            }
+
+            if (ContainsKnight(board, row - 1, col + 2))
+            {
+                count++;
+            }
+            if (ContainsKnight(board, row + 1, col - 2))
+            {
+                count++;
+            }
+            if (ContainsKnight(board, row + 1, col + 2))
+            {
+                count++;
+            }
+
+            if (ContainsKnight(board, row + 2, col - 1))
+            {
+                count++;
+            }
+            if (ContainsKnight(board, row + 2, col + 1))
+            {
+                count++;
+            }
+
+            return count;
+        }
+
+        private static bool ContainsKnight(char[,] board, int row, int col)
+        {
+            if (!IsValidCell(row, col, board.GetLength(0)))
+            {
+                return false;
+            }
+
+            return board[row, col] == 'K';
+        }
+
+        private static bool IsValidCell(int row, int col, int length)
+        {
+            return row >= 0 && row < length && col >= 0 && col < length;
+        }
+    }
+}
