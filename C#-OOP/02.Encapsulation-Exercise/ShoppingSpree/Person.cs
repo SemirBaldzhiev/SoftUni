5507079f396948b90f8226df56fbedc0fa98baca
Semir Baldzhiev <semir.baldzhiev@gmail.com>
2021-02-27 19:51:41 +0200

    Added task 3 from encapsulation exercises

---
+++ C#-OOP/02.Encapsulation-Exercise/ShoppingSpree/Person.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShoppingSpree
+{
+    public class Person
+    {
+        private const string EmptyBagMessage = "Nothing bought";
+        private string name;
+        private decimal money;
+        private List<string> bag;
+
+        public Person(string name, decimal money)
+        {
+            this.Name = name;
+            this.Money = money;
+            this.bag = new List<string>();
+        }
+
+        public string Name 
+        {
+            get { return this.name; }
+            set
+            {
+                if (string.IsNullOrEmpty(value) || string.IsNullOrWhiteSpace(value))
+                {
+                    throw new Exception("Name cannot be empty");
+                }
+
+                this.name = value;
+            }
+        }
+
+        public decimal Money
+        {
+            get { return this.money; }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new Exception("Money cannot be negative");
+                }
+                this.money = value;
+            }
+        }
+
+        public void Bought(Product product)
+        {
+            if (this.Money >= product.Cost)
+            {
+                this.Money -= product.Cost;
+                this.bag.Add(product.Name);
+                Console.WriteLine($"{this.Name} bought {product.Name}");
+            }
+            else
+            {
+                Console.WriteLine($"{this.Name} can't afford {product.Name}");
+            }
+            
+        }
+
+        public override string ToString()
+        {
+            string products = bag.Count == 0 ? EmptyBagMessage : string.Join(", ", bag);
+            return $"{this.Name} - {products}";
+        }
+    }
+}
---
+++ C#-OOP/02.Encapsulation-Exercise/ShoppingSpree/StartUp.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ShoppingSpree
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            
+            List<Person> people = new List<Person>();
+            List<Product> products = new List<Product>();
+
+            try
+            {
+                string[] peopleInput = Console.ReadLine().Split(";", StringSplitOptions.RemoveEmptyEntries).ToArray();
+                string[] productsInput = Console.ReadLine().Split(";", StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+                foreach (var person in peopleInput)
+                {
+                    string[] personData = person.Split("=");
+
+                    Person personToAdd = new Person(personData[0], decimal.Parse(personData[1]));
+
+
+                    people.Add(personToAdd);
+                }
+
+                foreach (var product in productsInput)
+                {
+                    string[] productData = product.Split("=");
+
+                    Product productToAdd = new Product(productData[0], decimal.Parse(productData[1]));
+
+                    products.Add(productToAdd);
+                }
+
+
+                while (true)
+                {
+                    string line = Console.ReadLine();
+
+                    if (line == "END")
+                    {
+                        break;
+                    }
+
+                    string[] commands = line.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                    string name = commands[0];
+                    string productName = commands[1];
+
+                    Person buyer = people.Find(x => x.Name == name);
+
+                    Product productToBuy = products.Find(x => x.Name == productName);
+
+                    if (buyer != null && productToBuy != null)
+                    {
+                        buyer.Bought(productToBuy);
+                    }
+
+                }
+
+                people.ForEach(p => Console.WriteLine(p));
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+        }
+    }
+}
