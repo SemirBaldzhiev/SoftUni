2c17006cfc731f022a5223085eccb3226a742640
Semir Baldzhiev <semir.baldzhiev@gmail.com>
2021-02-27 11:59:13 +0200

    Added task 6 from inheritance exercises

---
+++ C#-Advanced/10.Exam-Preparation/Bombs/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Bombs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> bombEffects = new Queue<int>(Console.ReadLine().Split(", ").Select(int.Parse).ToArray());
+            Stack<int> bombCasings = new Stack<int>(Console.ReadLine().Split(", ").Select(int.Parse).ToArray());
+
+
+
+
+        }
+    }
+}
---
+++ C#-OOP/01.Inheritance-Exercise/Animals/StartUp.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Animals
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            while (input != "Beast!")
+            {
+                string animal = input;
+                string[] animalData = Console.ReadLine().Split();
+
+                if (string.IsNullOrEmpty(animalData[0]) || string.IsNullOrEmpty(animalData[2]) || int.Parse(animalData[1]) < 0)
+                {
+                    Console.WriteLine("Invalid input!");
+                    input = Console.ReadLine();
+                    continue;
+                }
+
+                switch (animal)
+                {
+                    case "Dog":
+                        Dog dog = new Dog(animalData[0], int.Parse(animalData[1]), animalData[2]);
+                        Console.WriteLine(dog);
+                        break;
+                    case "Cat":
+                        Cat cat = new Cat(animalData[0], int.Parse(animalData[1]), animalData[2]);
+                        Console.WriteLine(cat);
+                        break;
+                    case "Frog":
+                        Frog frog = new Frog(animalData[0], int.Parse(animalData[1]), animalData[2]);
+                        Console.WriteLine(frog);
+                        break;
+                    case "Kitten":
+                        Kitten kitten = new Kitten(animalData[0], int.Parse(animalData[1]));
+                        Console.WriteLine(kitten);
+                        break;
+                    case "Tomcat":
+                        Tomcat tomcat = new Tomcat(animalData[0], int.Parse(animalData[1]));
+                        Console.WriteLine(tomcat);
+                        break;
+                    
+                }
+
+                input = Console.ReadLine();
+            }
+        }
+    }
+}
