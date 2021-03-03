7bce59740aeebc20e9555f55cfa780be415baeec
Semir Baldzhiev <semir.baldzhiev@gmail.com>
2021-03-03 14:47:10 +0200

    Added task 4 from encapsulation exercise

---
+++ C#-OOP/02.Encapsulation-Exercise/PizzaCalories/StartUp.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PizzaCalories
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            string pizzaName = Console.ReadLine().Split()[1];
+            string[] doughData = Console.ReadLine().Split().ToArray();
+
+            string flourType = doughData[1];
+            string bakingTechnique = doughData[2];
+            int weight = int.Parse(doughData[3]);
+
+            try
+            {
+                Dough dough = new Dough(flourType, bakingTechnique, weight);
+
+                Pizza pizza = new Pizza(pizzaName, dough);
+
+                while (true)
+                {
+                    string line = Console.ReadLine();
+
+                    if (line == "END")
+                    {
+                        break;
+                    }
+
+                    string[] toppingData = line.Split().ToArray();
+
+                    string toppingName = toppingData[1];
+                    int toppingWeight = int.Parse(toppingData[2]);
+
+                    Topping topping = new Topping(toppingName, toppingWeight);
+
+                    pizza.AddTopping(topping);
+                }
+
+                Console.WriteLine($"{pizza.Name} - {pizza.GetCalories():F2} Calories.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ C#-OOP/02.Encapsulation-Exercise/PizzaCalories/Topping.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PizzaCalories
+{
+    public class Topping
+    {
+        private const int MinWeight = 1;
+        private const int MaxWeight = 50;
+        private const string InvalidWeight = "{0} weight should be in the range [1..50].";
+
+        private string name;
+        private int weight;
+
+        public Topping(string name, int weight)
+        {
+            this.Name = name;
+            this.Weight = weight;
+        }
+
+        public string Name
+        {
+            get => this.name;
+            private set
+            {
+                Validator.ThrowIfValueIsNotAllowed(new HashSet<string> { "meat", "veggies", "cheese", "sauce" }, value.ToLower(), $"Cannot place {value} on top of your pizza.");
+               
+
+                this.name = value;
+            }
+        }
+
+        public int Weight
+        {
+            get => this.weight;
+            private set
+            {
+                //Validator.ThrowIfNumberIsNotInRange(MinWeight, MaxWeight, value, $"{this.name} weight should be in the range[{MinWeight}..{MaxWeight}].");
+                if (value < MinWeight ||value > MaxWeight)
+                {
+                    throw new ArgumentException(String.Format(InvalidWeight, this.name));
+                }
+                
+                this.weight = value;
+            }
+        }
+
+        public double GetCalories()
+        {
+            double modifier = GetModifier();
+
+            return this.Weight * 2 * modifier;
+        }
+
+        private double GetModifier()
+        {
+            string nameLower = this.Name.ToLower();
+
+            if (nameLower == "meat")
+            {
+                return 1.2;
+            }
+            else if (nameLower == "veggies")
+            {
+                return 0.8;
+            }
+            else if (nameLower == "cheese")
+            {
+                return 1.1;
+            }
+
+            return 0.9;
+        }
+    }
+}
