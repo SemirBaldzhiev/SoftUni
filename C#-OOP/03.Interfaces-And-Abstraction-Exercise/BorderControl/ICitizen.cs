c7ae1ac22837dd62bf1c96159a3ff3110876bc6d
Semir Baldzhiev <semir.baldzhiev@gmail.com>
2021-03-09 17:48:43 +0200

    Added task 4 from abstraction exercises

---
+++ C#-OOP/03.Interfaces-And-Abstraction-Exercise/BorderControl/ICitizen.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BorderControl
+{
+    public interface ICitizen : IIdentifiable
+    {
+        string Name { get; set; }
+        int Age { get; set; }
+    }
+}
