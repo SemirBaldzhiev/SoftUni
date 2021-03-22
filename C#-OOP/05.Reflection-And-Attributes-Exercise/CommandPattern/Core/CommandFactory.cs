83bc7251ad4193d34cc25c2fbc4ae065101f440a
Semir Baldzhiev <semir.baldzhiev@gmail.com>
2021-03-22 23:04:32 +0200

    Added solution for reflection exercise

---
+++ C#-OOP/05.Reflection-And-Attributes-Exercise/CommandPattern/Core/CommandFactory.cs
@@ -0,0 +1,26 @@
+﻿using CommandPattern.Core.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace CommandPattern.Core
+{
+    public class CommandFactory : ICommandFactory
+    {
+        public ICommand CreateCommand(string commandType)
+        {
+            var type = Assembly.GetEntryAssembly().GetTypes().FirstOrDefault(c => c.Name.StartsWith(commandType));
+
+            if (type == null)
+            {
+                throw new ArgumentException($"{type} is invalid command type.");
+            }
+
+            var instance = (ICommand)Activator.CreateInstance(type);
+
+            return instance; 
+        }
+    }
+}
