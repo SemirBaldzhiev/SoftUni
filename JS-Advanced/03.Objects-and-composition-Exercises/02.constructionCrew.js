765f2c74a2940f497830e483bd72656fe4e5bda2
Semir Baldzhiev <semir.baldzhiev@gmail.com>
2021-06-05 16:25:13 +0300

    Added task 2 from object exercises

---
+++ JS-Advanced/03.Objects-and-composition-Exercises/02.constructionCrew.js
@@ -0,0 +1,14 @@
+function solve(obj){
+    if (obj.dizziness === true) {
+        obj.levelOfHydrated += 0.1 * obj.weight * obj.experience;
+        obj.dizziness = false;
+    }
+
+    return obj;
+}
+
+console.log(solve({ weight: 80,
+    experience: 1,
+    levelOfHydrated: 0,
+    dizziness: true }
+  ))
