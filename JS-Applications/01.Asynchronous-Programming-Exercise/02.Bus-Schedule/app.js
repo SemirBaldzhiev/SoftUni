7a72bf2f01581a9b725cdd0294a4a82fb9ba43ec
Semir Baldzhiev <semir.baldzhiev@gmail.com>
2021-07-05 15:40:13 +0300

    Added task 2 from exrcises

--- JS-Applications/01.Asynchronous-Programming-Exercise/02.Bus-Schedule/app.js
+++ JS-Applications/01.Asynchronous-Programming-Exercise/02.Bus-Schedule/app.js
@@ -1,11 +1,29 @@
 function solve() {
+    let baseUrl = 'http://localhost:3030/jsonstore/bus/schedule';
+    let info = document.querySelector('.info');
+    let departButton = document.getElementById('depart');
+    let arriveButton = document.getElementById('arrive');
+    
+    let currId = 'depot';
+    let currStopName = '';
 
     function depart() {
-        console.log('Depart TODO...');
+        fetch(`${baseUrl}/${currId}`)
+            .then(res => res.json())
+            .then(stop => {
+                currStopName = stop.name;
+                info.textContent = `Next stop ${currStopName}`;
+                currId = stop.next;
+            });
+
+        departButton.setAttribute('disabled', 'true');
+        arriveButton.disabled = false;
     }
 
     function arrive() {
-        console.log('Arrive TODO...');
+        info.textContent = `Arriving at ${currStopName}`;
+        departButton.disabled = false;
+        arriveButton.disabled = true;;
     }
 
     return {
