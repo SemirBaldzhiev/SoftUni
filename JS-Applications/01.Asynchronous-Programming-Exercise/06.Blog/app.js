1db67d25ef6f5d0ab48263d1b50305073a0f25bf
Semir Baldzhiev <semir.baldzhiev@gmail.com>
2021-07-12 19:09:17 +0300

    Added task 6 from exercise

--- JS-Applications/01.Asynchronous-Programming-Exercise/06.Blog/app.js
+++ JS-Applications/01.Asynchronous-Programming-Exercise/06.Blog/app.js
@@ -1,5 +1,75 @@
 function attachEvents() {
-    console.log('TODO...');
+
+    let loadPostsBtn = document.getElementById('btnLoadPosts');
+
+    loadPostsBtn.addEventListener('click', loadPostsHandler)
+
+    let viewPostBtn = document.getElementById('btnViewPost');
+
+    viewPostBtn.addEventListener('click', viewPostHandler);
+
+    async function loadPostsHandler(e){
+        let postsRequest = await fetch('http://localhost:3030/jsonstore/blog/posts');
+        let posts = await postsRequest.json();
+        
+        let selectElement = document.getElementById('posts');
+        removeChildElements(selectElement);
+
+
+        Object.keys(posts).forEach(key => {
+            let optionElement = document.createElement('option');
+            optionElement.value = key;
+            optionElement.textContent = posts[key].title;
+
+            selectElement.appendChild(optionElement);
+
+        });
+    }
+
+    async function viewPostHandler(e){
+
+        let id = e.target.previousElementSibling.value;
+
+        if (!id) {
+            return;
+        }
+
+        let postRequest = await fetch(`http://localhost:3030/jsonstore/blog/posts/${id}`);
+
+        let post = await postRequest.json();
+
+        let h1 = document.getElementById('post-title');
+        h1.textContent = post.title;
+
+        let ulPostBody = document.getElementById('post-body');
+        ulPostBody.textContent = post.body;
+
+        let commentsRequest = await fetch('http://localhost:3030/jsonstore/blog/comments');
+
+        let comments = await commentsRequest.json();
+
+       let ulComments = document.getElementById('post-comments');
+
+       removeChildElements(ulComments);
+
+        Object.keys(comments).forEach(key => {
+            if (comments[key].postId == id) {
+                let liElement = document.createElement('li');
+
+                liElement.textContent = comments[key].text;
+                ulComments.appendChild(liElement);
+            }
+        })
+
+
+    }
+
+    function removeChildElements(parent){
+        while(parent.firstChild){
+            parent.removeChild(parent.firstChild);
+        }
+    }
+
 }
 
 attachEvents();
