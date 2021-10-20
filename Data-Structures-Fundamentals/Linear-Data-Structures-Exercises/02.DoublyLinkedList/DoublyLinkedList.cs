af0a31ba554e0e6806af777176855eb6bedd1d48
Semir Baldzhiev <semir.baldzhiev@gmail.com>
2021-10-20 20:00:09 +0300

    Added doubly linked list implementation

--- Data-Structures-Fundamentals/Linear-Data-Structures-Exercises/02.DoublyLinkedList/DoublyLinkedList.cs
+++ Data-Structures-Fundamentals/Linear-Data-Structures-Exercises/02.DoublyLinkedList/DoublyLinkedList.cs
@@ -7,47 +7,103 @@
     public class DoublyLinkedList<T> : IAbstractLinkedList<T>
     {
         private Node<T> head;
+        private Node<T> tail;
+
 
         public int Count { get; private set; }
 
         public void AddFirst(T item)
         {
-            throw new NotImplementedException();
+            var newNode = new Node<T>(item, null, null);
+            
+            if (Count == 0)
+	        {
+                head = newNode;
+                tail = newNode;
+                Count++;
+                return;
+	        }
+
+            var oldHead = head;
+            oldHead.Previous = newNode;
+            head = newNode;
+            head.Next = oldHead;
+
         }
 
         public void AddLast(T item)
         {
-            throw new NotImplementedException();
+           var newNode = new Node<T>(item, null, null);
+            
+            if (Count == 0)
+	        {
+                head = newNode;
+                tail = newNode;
+                Count++;
+                return;
+	        }
+
+            var oldTail = tail;
+            oldTail.Next = newNode;
+            tail = newNode;
+            tail.Previous = oldTail;
         }
 
         public T GetFirst()
         {
-            throw new NotImplementedException();
+            ValidateCollection();
+            return head.Item;
         }
 
         public T GetLast()
         {
-            throw new NotImplementedException();
+            ValidateCollection();
+            return tail.Item;
         }
 
         public T RemoveFirst()
         {
-            throw new NotImplementedException();
+            ValidateCollection();
+
+            var oldHead = head;
+            head = head.Next;
+
+            Count--;
+
+            return oldHead.Item;
         }
 
         public T RemoveLast()
         {
-            throw new NotImplementedException();
+            ValidateCollection();
+           
+            var oldTail = tail;
+            tail = tail.Previous;
+
+            Count--;
+            return oldTail.Item;
         }
 
         public IEnumerator<T> GetEnumerator()
         {
-            throw new NotImplementedException();
+            var current = this.head;
+            while (current != null)
+            {
+                yield return current.Item;
+                current = current.Next;
+            }
         }
 
         IEnumerator IEnumerable.GetEnumerator()
+            => this.GetEnumerator();
+
+        private void ValidateCollection()
         {
-            throw new NotImplementedException();
+            if (Count == 0)
+            {
+                throw new InvalidOperationException();
+            }
+	
         }
     }
 }
