5d8c154bc1b9b111f2cfc0e11ce8dd8f68e41626
Semir Baldzhiev <semir.baldzhiev@gmail.com>
2021-11-01 16:35:38 +0200

    Added task 1 from exercises

---
+++ Entity-Framework-Core/05.LINQ-Exercises/MusicHub/Data/MusicHubDbContext.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MusicHub.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace MusicHub.Data
+{
+    public class MusicHubDbContext : DbContext
+    {
+        public MusicHubDbContext()
+        {
+
+        }
+        public MusicHubDbContext([NotNull] DbContextOptions options) 
+            : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(@"Server=DESKTOP-F0J1M25\SQLEXPRESS;Database=FootballBetting;Integrated Security=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<SongPerformer>(e =>
+            {
+                e.HasKey(sp => new { sp.SongId, sp.PerformerId });
+            });
+        }
+    }
+}
---
+++ Entity-Framework-Core/05.LINQ-Exercises/MusicHub/StartUp.cs
@@ -0,0 +1,26 @@
+﻿using MusicHub.Data;
+using MusicHub.Initializer;
+using System;
+
+namespace MusicHub
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            MusicHubDbContext context =
+                new MusicHubDbContext();
+
+            DbInitializer.ResetDatabase(context);
+        }
+        public static string ExportAlbumsInfo(MusicHubDbContext context, int producerId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static string ExportSongsAboveDuration(MusicHubDbContext context, int duration)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
