7bdaacf2b0fbe578b3846ce82e699e84af8a0540
Semir Baldzhiev <semir.baldzhiev@gmail.com>
2021-11-26 17:51:46 +0200

    Added task 1 from car dealer exercises

---
+++ Entity-Framework-Core/08.Json-Processing-Exercises/CarDealer/DTO/Input/SupplierInputDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarDealer.DTO.Input
+{
+    public class SupplierInputDto
+    {
+        public string Name { get; set; }
+        public bool IsImporter { get; set; }
+    }
+}
---
+++ Entity-Framework-Core/08.Json-Processing-Exercises/CarDealer/StartUp.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using AutoMapper;
+using CarDealer.Data;
+using CarDealer.DTO.Input;
+using CarDealer.Models;
+using Newtonsoft.Json;
+
+namespace CarDealer
+{
+
+    public class StartUp
+    {
+        private static IMapper mapper;
+
+        public static void Main(string[] args)
+        {
+            var context = new CarDealerContext();
+
+            context.Database.EnsureDeleted();
+            context.Database.EnsureCreated();
+
+            string suppliersJson = File.ReadAllText("../../../Datasets/suppliers.json");
+
+            string suppliersResult = ImportSuppliers(context, suppliersJson);
+
+            Console.WriteLine(suppliersResult);
+        }
+
+        // Import Problems
+        public static string ImportSuppliers(CarDealerContext context, string inputJson)
+        {
+            IEnumerable<SupplierInputDto> suppliers = JsonConvert.DeserializeObject<IEnumerable<SupplierInputDto>>(inputJson);
+
+            InitializeMapper();
+
+            var mappedSuppliers = mapper.Map<IEnumerable<Supplier>>(suppliers);
+
+            context.Suppliers.AddRange(mappedSuppliers);
+            context.SaveChanges();
+
+            return $"Successfully imported {mappedSuppliers.Count()}.";
+        }
+
+
+        public static void InitializeMapper()
+        {
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile<CarDealerProfile>();
+            });
+
+            mapper = new Mapper(config);
+        }
+    }
+}
