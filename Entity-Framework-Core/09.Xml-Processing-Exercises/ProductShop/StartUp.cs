0bc4438af08f1b7afcb6f53d3f16c2b9e9e9c457
Semir Baldzhiev <semir.baldzhiev@gmail.com>
2021-11-29 23:07:30 +0200

    Added tasks from xml processing exercises

---
+++ Entity-Framework-Core/09.Xml-Processing-Exercises/ProductShop/StartUp.cs
@@ -0,0 +1,49 @@
+﻿using ProductShop.Data;
+using ProductShop.Dtos.Import;
+using ProductShop.Models;
+using System;
+using System.IO;
+using System.Linq;
+using System.Xml.Serialization;
+
+namespace ProductShop
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            var context = new ProductShopContext();
+
+            string usersXml = File.ReadAllText("../../../Datasets/users.xml");
+
+            Console.WriteLine(ImportUsers(context, usersXml));
+        }
+
+        public static string ImportUsers(ProductShopContext context, string inputXml)
+        {
+            int count = 0;
+            using (var reder = new StringReader(inputXml))
+            {
+                var serializer = new XmlSerializer(typeof(ImportUserDto[]), new XmlRootAttribute("Users"));
+
+                var users = (ImportUserDto[])serializer.Deserialize(reder);
+
+                var mappedUsers = users
+                    .Select(u => new User
+                    {
+                        FirstName = u.FirstName,
+                        LastName = u.LastName,
+                        Age = u.Age
+                    })
+                    .ToList();
+
+                context.Users.AddRange(mappedUsers);
+                context.SaveChanges();
+
+                count = mappedUsers.Count();
+            }
+
+            return $"Successfully imported {count}";
+        }
+    }
+}
