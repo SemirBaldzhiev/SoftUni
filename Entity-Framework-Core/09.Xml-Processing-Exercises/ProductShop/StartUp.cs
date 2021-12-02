ab6ce83e2b105cd0412168d797f2c50ec098df20
Semir Baldzhiev <semir.baldzhiev@gmail.com>
2021-12-02 17:38:22 +0200

    Added category and product import

--- Entity-Framework-Core/09.Xml-Processing-Exercises/ProductShop/StartUp.cs
+++ Entity-Framework-Core/09.Xml-Processing-Exercises/ProductShop/StartUp.cs
@@ -45,5 +45,88 @@ namespace ProductShop
 
             return $"Successfully imported {count}";
         }
+
+        public static string ImportProducts(ProductShopContext context, string inputXml)
+        {
+            int count = 0;
+            using (var reader = new StringReader(inputXml))
+            {
+                var serializer = new XmlSerializer(typeof(ImportProductDto[]), new XmlRootAttribute("Products"));
+
+                var products = (ImportProductDto[])serializer.Deserialize(reader);
+
+                var mappedProducts = products
+                    .Select(u => new Product
+                    {
+                        Name = u.Name, 
+                        Price = u.Price,
+                        SellerId = u.SellerId,
+                        BuyerId = u.BuyerId
+                    })
+                    .ToList();
+
+                context.Products.AddRange(mappedProducts);
+                context.SaveChanges();
+
+                count = mappedProducts.Count();
+            }
+
+
+            return $"Successfully imported {count}";
+        }
+        public static string ImportCategories(ProductShopContext context, string inputXml)
+        {
+
+            int count = 0;
+            using (var reader = new StringReader(inputXml))
+            {
+                var serializer = new XmlSerializer(typeof(ImportCategoryDto[]), new XmlRootAttribute("Categories"));
+
+                var categories = (ImportCategoryDto[])serializer.Deserialize(reader);
+
+                var mappedCategories = categories
+                    .Select(u => new Category
+                    {
+                        Name = u.Name
+                    })
+                    .ToList();
+
+                context.Categories.AddRange(mappedCategories);
+                context.SaveChanges();
+
+                count = mappedCategories.Count();
+            }
+            return $"Successfully imported {count}";
+        }
+
+        public static string ImportCategoryProducts(ProductShopContext context, string inputXml)
+        {
+            int count = 0;
+            using (var reader = new StringReader(inputXml))
+            {
+                var serializer = new XmlSerializer(typeof(ImportUserDto[]), new XmlRootAttribute("Users"));
+
+                var users = (ImportUserDto[])serializer.Deserialize(reader);
+
+                var mappedUsers = users
+                    .Select(u => new User
+                    {
+                        FirstName = u.FirstName,
+                        LastName = u.LastName,
+                        Age = u.Age
+                    })
+                    .ToList();
+
+                context.Users.AddRange(mappedUsers);
+                context.SaveChanges();
+
+                count = mappedUsers.Count();
+            }
+
+            return $"Successfully imported {count}";
+        }
+
+            return $"Successfully imported {count}";
+        }
     }
 }
