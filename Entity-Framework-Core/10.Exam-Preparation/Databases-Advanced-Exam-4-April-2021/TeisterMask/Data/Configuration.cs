10e65667ce9878efda569f3faf33a0079fa38beb
Semir Baldzhiev <semir.baldzhiev@gmail.com>
2021-12-03 21:30:04 +0200

    Added tasks from exam prep

---
+++ Entity-Framework-Core/10.Exam-Preparation/Databases-Advanced-Exam-4-April-2021/TeisterMask/Data/Configuration.cs
@@ -0,0 +1,7 @@
+﻿namespace TeisterMask.Data
+{
+    public static class Configuration
+    {
+        public static string ConnectionString = @"Server=DESKTOP-F0J1M25\SQLEXPRESS;Database=TeisterMask;Trusted_Connection=True";
+    }
+}
---
+++ Entity-Framework-Core/10.Exam-Preparation/Databases-Advanced-Exam-4-April-2021/TeisterMask/DataProcessor/ImportDto/ImportProjectTasksDto.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace TeisterMask.DataProcessor.ImportDto
+{
+    [XmlType("Task")]
+    public class ImportProjectTasksDto
+    {
+        [XmlElement("Name")]
+        [Required]
+        [MinLength(2)]
+        [MaxLength(40)]
+        public string Name { get; set; }
+        [Required]
+        [XmlElement("OpenDate")]
+        public string TaskOpenDate { get; set; }
+
+        [Required]
+        [XmlElement("DueDate")]
+        public string TaskDueDate { get; set; }
+
+        [XmlElement("ExecutionType")]
+        [Range(0, 3)]
+        public int ExecutionType { get; set; }
+
+        [XmlElement("LabelType")]
+        [Range(0, 4)]
+        public int LabelType { get; set; }
+    }
+}
