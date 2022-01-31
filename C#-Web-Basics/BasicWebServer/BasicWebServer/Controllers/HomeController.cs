52d27c3f6ab18c3e89b9d8c21ae8c1c3cf995995
Semir Baldzhiev <semir.baldzhiev@gmail.com>
2022-01-31 12:36:28 +0200

    Added HomeController class and actions

---
+++ C#-Web-Basics/BasicWebServer/BasicWebServer/Controllers/HomeController.cs
@@ -0,0 +1,128 @@
+﻿using BasicWebServer.Server.Controllers;
+using BasicWebServer.Server.HTTP;
+using System.Text;
+using System.Web;
+
+namespace BasicWebServer.Controllers
+{
+    public class HomeController : Controller
+    {
+        private const string HtmlForm = @"<form action='/HTML' method='POST'>
+                Name: <input type='text' name='Name'/>
+                Age: <input type='number' name ='Age'/>
+                <input type='submit' value ='Save' />
+                </form>";
+
+        private const string DownloadForm = @"<form action='/Content' method='POST'>
+                <input type='submit' value ='Download Sites Content' /> 
+                </form>";
+
+        private const string FileName = "content.txt";
+        public HomeController(Request request) 
+            : base(request)
+        {
+        }
+
+        public Response Index() => Text("Hello from the server!");
+
+        public Response Redirect() => Redirect("https://softuni.org/");
+
+        public Response Html() => Html(HomeController.HtmlForm);
+
+        public Response HtmlFormPost()
+        {
+            string formdata = string.Empty;
+
+            foreach (var (key, value) in Request.Form)
+            {
+                formdata += $"{key} - {value}";
+                formdata += Environment.NewLine;
+            }
+
+            return Text(formdata);
+        }
+
+        public Response Content() => Html(HomeController.DownloadForm);
+
+        public Response DownloadContent()
+        {
+            DownloadSitesAsTextFile(HomeController.FileName,
+                new string[] { "https://judge.softuni.org/", "https://softuni.org/" }).Wait();
+
+            return File(HomeController.FileName);
+        }
+
+        public Response Cookies()
+        {
+            if (Request.Cookies.Any(c => c.Name != BasicWebServer.Server.HTTP.Session.SessionCookieName))
+            {
+                var cookieText = new StringBuilder();
+
+                cookieText.AppendLine("<h1>Cookies</h1>");
+
+                cookieText.Append("<table> border='1'><tr><th>Name</th><th>Value</th></tr>");
+
+                foreach (var cookie in Request.Cookies)
+                {
+                    cookieText.Append("<tr>");
+                    cookieText.Append($"<td>{HttpUtility.HtmlEncode(cookie.Name)}</td>");
+                    cookieText.Append($"<td>{HttpUtility.HtmlEncode(cookie.Value)}</td>");
+                    cookieText.Append("</tr>");
+                }
+
+                cookieText.Append("</table>");
+
+                return Html(cookieText.ToString());
+            }
+
+            var cookies = new CookieCollection();
+
+            cookies.Add("My-Cookie", "My-Value");
+            cookies.Add("My-Second-Cookie", "My-Second-Value");
+
+            return Html("<h1>Cookies set!</h1>", cookies);
+        }
+
+        public Response Session()
+        {
+            string currentDateKey = "CurrentDate";
+            var sessionExist = Request.Session.Contains(currentDateKey);
+
+            if (sessionExist)
+            {
+                var currentDate = Request.Session[currentDateKey];
+                return Html($"Stored date: {currentDate}"!);
+            }
+
+            return Html("Current date stored!");
+        }
+        
+
+        private static async Task DownloadSitesAsTextFile(string fileName, string[] urls)
+        {
+            var downloads = new List<Task<string>>();
+
+            foreach (var url in urls)
+            {
+                downloads.Add(DownloadWebSiteContent(url));
+            }
+
+            var responses = await Task.WhenAll(downloads);
+
+            var responsesString = string.Join(Environment.NewLine + new string('-', 100), responses);
+
+            await System.IO.File.WriteAllTextAsync(fileName, responsesString);
+        }
+
+
+        private static async Task<string> DownloadWebSiteContent(string url)
+        {
+            using var httpClient = new HttpClient();
+            var response = await httpClient.GetAsync(url);
+            var html = await response.Content.ReadAsStringAsync();
+
+            return html.Substring(0, 2000);
+        }
+
+    }
+}
