c76b6ab2fa263c0663cc78b8a89c414c26d6e359
Semir Baldzhiev <semir.baldzhiev@gmail.com>
2022-02-01 22:57:31 +0200

    Add data models

--- C#-Web-Basics/ExamPreparation/Shared-Trip-Exam/SharedTrip/Models/Trip.cs
+++ C#-Web-Basics/ExamPreparation/Shared-Trip-Exam/SharedTrip/Models/Trip.cs
@@ -1,7 +1,37 @@
-﻿namespace SharedTrip.Models
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace SharedTrip.Models
 {
     public class Trip
     {
+        public Trip()
+        {
+            UserTrips = new HashSet<UserTrip>();
+        }
 
+        [Key]
+        public string Id { get; set; }
+
+        [Required]
+        public string StartPoint { get; set; }
+
+        [Required]
+        public string EndPoint { get; set; }
+
+        [Required]
+        public DateTime DepartureTime { get; set; }
+
+        [Required]
+        public int Seats { get; set; }
+
+        [Required]
+        [MaxLength(80)]
+        public string  Description { get; set; }
+
+        public string ImagePath { get; set; }
+
+        public ICollection<UserTrip> UserTrips { get; set; }
     }
 }
--- C#-Web-Basics/ExamPreparation/Shared-Trip-Exam/SharedTrip/Models/User.cs
+++ C#-Web-Basics/ExamPreparation/Shared-Trip-Exam/SharedTrip/Models/User.cs
@@ -1,7 +1,28 @@
-﻿namespace SharedTrip.Models
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace SharedTrip.Models
 {
     public class User
     {
-        
+        public User()
+        {
+            UserTrips = new HashSet<UserTrip>();
+        }
+
+        [Key]
+        public string Id { get; set; }
+
+        [Required]
+        [MaxLength(20)]
+        public string Username { get; set; }
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        [MaxLength(20)]
+        public string Password { get; set; }
+
+        public ICollection<UserTrip> UserTrips { get; set; }
     }
 }
