34ec85e17741a1de31e0efa451478439cae226bd
Semir Baldzhiev <semir.baldzhiev@gmail.com>
2022-02-03 13:47:35 +0200

    Added data constants

---
+++ C#-Web-Basics/ExamPreparation/Shared-Trip-Exam/SharedTrip/Constants/DataConstants.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedTrip.Constants
+{
+    public static class DataConstants
+    {
+        public const int UsernameMinLength = 5;
+        public const int DefaultMaxLength = 20;
+        public const int PasswordMinLength = 6;
+        public const int SeatsMinValue = 2;
+        public const int SeatsMaxValue = 6;
+        public const int DescriptionMaxLength = 80;
+        public const string EmailRegularExpression = @"^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$";
+    }
+}
