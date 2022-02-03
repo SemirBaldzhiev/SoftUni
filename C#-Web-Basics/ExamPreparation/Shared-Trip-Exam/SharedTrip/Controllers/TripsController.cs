62d76d5321762ca676d2eaf434234815f5af04a4
Semir Baldzhiev <semir.baldzhiev@gmail.com>
2022-02-03 13:46:03 +0200

    Implemented controllers actions

--- C#-Web-Basics/ExamPreparation/Shared-Trip-Exam/SharedTrip/Controllers/TripsController.cs
+++ C#-Web-Basics/ExamPreparation/Shared-Trip-Exam/SharedTrip/Controllers/TripsController.cs
@@ -1,14 +1,123 @@
 ﻿using MyWebServer.Controllers;
+using MyWebServer.Http;
+using SharedTrip.Data;
+using SharedTrip.Dtos.Trips;
+using SharedTrip.Models;
+using SharedTrip.Services;
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SharedTrip.Controllers
 {
     public class TripsController : Controller
     {
+        private readonly IValidator validator;
+        private readonly ApplicationDbContext data;
+        public TripsController(IValidator validator, ApplicationDbContext data)
+        {
+            this.validator = validator;
+            this.data = data;
+        }
 
+        [Authorize]
+        public HttpResponse All()
+        {
+            var allTrips = this.data
+                .Trips
+                .Select(trip => new AllTripsViewModel
+                {
+                    Id = trip.Id,
+                    StartPoint = trip.StartPoint,
+                    EndPoint = trip.EndPoint,
+                    Description = trip.Description,
+                    Seats = trip.Seats,
+                    DepartureTime = trip.DepartureTime.ToString("dd.MM.yyyy HH:mm"),
+                    ImagePath = trip.ImagePath
+                })
+                .ToList();
+
+            return View(allTrips);
+        }
+
+        [Authorize]
+        public HttpResponse Add() => View();
+
+        [Authorize]
+        [HttpPost]
+        public HttpResponse Add(AddTripFormModel model)
+        {
+            var modelErrors = this.validator.ValidateTrip(model);
+
+            if (modelErrors.Any())
+            {
+                return Error(modelErrors);
+            }
+
+            var trip = new Trip
+            {
+                StartPoint = model.StartPoint,
+                EndPoint = model.EndPoint,
+                Seats = model.Seats,
+                Description = model.Description,
+                DepartureTime = model.DepartureTime,
+                ImagePath = model.ImagePath
+            };
+
+
+            this.data.Trips.Add(trip);
+
+            this.data.SaveChanges();
+
+            return Redirect("/Trips/All");
+        }
+
+        [Authorize]
+        public HttpResponse Details(string tripId)
+        {
+            var tripDetails = this.data
+                .Trips
+                .Where(t => t.Id == tripId)
+                .Select(t => new TripDetailsViewModel
+                {
+                    Id = t.Id,
+                    StartPoint = t.StartPoint,
+                    EndPoint = t.EndPoint,
+                    Description = t.Description,
+                    DepartureTime = t.DepartureTime.ToString("yyyy-MM-ddTHH:mm"),
+                    Seats = t.Seats,
+                    ImagePath = t.ImagePath
+                })
+                .FirstOrDefault();
+
+            if (tripDetails == null)
+            {
+                return NotFound();
+            }
+
+            return View(tripDetails);
+        }
+
+        [Authorize]
+        public HttpResponse AddUserToTrip(string tripId)
+        {
+            var trip = this.data.Trips.Find(tripId);
+
+            if (trip == null)
+            {
+                return NotFound();
+            }
+
+            var userTrip = new UserTrip
+            {
+                TripId = trip.Id,
+                UserId = this.User.Id
+            };
+
+            this.data.UserTrips.Add(userTrip);
+
+            this.data.SaveChanges();
+
+            return Redirect("/Trips/All");
+        }
     }
 }
--- C#-Web-Basics/ExamPreparation/Shared-Trip-Exam/SharedTrip/Controllers/UsersController.cs
+++ C#-Web-Basics/ExamPreparation/Shared-Trip-Exam/SharedTrip/Controllers/UsersController.cs
@@ -1,14 +1,82 @@
 ﻿using MyWebServer.Controllers;
-using System;
-using System.Collections.Generic;
+using MyWebServer.Http;
+using SharedTrip.Data;
+using SharedTrip.Dtos.Users;
+using SharedTrip.Models;
+using SharedTrip.Services;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SharedTrip.Controllers
 {
     public class UsersController : Controller
     {
+        private readonly IValidator validator;
+        private readonly ApplicationDbContext data;
+        private readonly IPasswordHasher passwordHasher;
 
+
+        public UsersController(IValidator validator, IPasswordHasher passwordHasher, ApplicationDbContext data)
+        {
+            this.validator = validator;
+            this.passwordHasher = passwordHasher;
+            this.data = data;
+        }
+
+        public HttpResponse Register() => View();
+
+        [HttpPost]
+        public HttpResponse Register(RegisterUserFormModel model)
+        {
+            var modelErrors = this.validator.ValidateUser(model);
+
+            if (modelErrors.Any())
+            {
+                return Error(modelErrors);
+            }
+
+            var user = new User
+            {
+                Username = model.Username,
+                Email = model.Email,
+                Password = passwordHasher.HashPassword(model.Password),
+            };
+
+            this.data.Users.Add(user);
+
+            this.data.SaveChanges();
+
+            return Redirect("/Users/Login");
+        }
+
+        public HttpResponse Login() => View();
+
+        [HttpPost]
+        public HttpResponse Login(LoginUserFormModel model)
+        {
+            var hashedPassword = passwordHasher.HashPassword(model.Password);
+
+            var userId = this.data
+                .Users
+                .Where(u => u.Username == model.Username && u.Password == hashedPassword)
+                .Select(u => u.Id)
+                .FirstOrDefault();
+
+            if (userId == null)
+            {
+                return Error("Username and password combination is not valid!");
+            }
+
+            this.SignIn(userId);
+
+            return Redirect("/Trips/All");
+        }
+       
+        [Authorize]
+        public HttpResponse Logout()
+        {
+            this.SignOut();
+
+            return Redirect("/");
+        }
     }
 }
