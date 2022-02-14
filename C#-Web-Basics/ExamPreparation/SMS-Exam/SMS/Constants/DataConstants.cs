6ccd6df3ccda1f8da21b7934d1b886f7b1725142
Semir Baldzhiev <semir.baldzhiev@gmail.com>
2022-02-14 20:33:24 +0200

    Added constants for price and user password

--- C#-Web-Basics/ExamPreparation/SMS-Exam/SMS/Constants/DataConstants.cs
+++ C#-Web-Basics/ExamPreparation/SMS-Exam/SMS/Constants/DataConstants.cs
@@ -9,5 +9,12 @@ namespace SMS.Constants
     public class DataConstants
     {
         public const int DefaultMaxLength = 20;
+        public const int UsernameMinLength = 5;
+        public const int PasswordMinLength = 6;
+        public const int ProductNameMinLength = 4;
+        public const decimal ProductMinPrice = 0.05m;
+        public const decimal ProductMaxPrice = 1000m;
+
+        public const string EmailRegularExpression = @"^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$";
     }
 }
