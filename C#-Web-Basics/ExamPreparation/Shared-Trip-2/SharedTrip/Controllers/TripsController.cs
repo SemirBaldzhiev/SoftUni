ca93aeb73d72f74bc4ddbed3da3e8d29ed614d11
Semir Baldzhiev <semir.baldzhiev@gmail.com>
2022-02-18 20:11:54 +0200

    Implemented trip controller

--- C#-Web-Basics/ExamPreparation/Shared-Trip-2/SharedTrip/Controllers/TripsController.cs
+++ C#-Web-Basics/ExamPreparation/Shared-Trip-2/SharedTrip/Controllers/TripsController.cs
@@ -1,5 +1,9 @@
-﻿using BasicWebServer.Server.Controllers;
+﻿using BasicWebServer.Server.Attributes;
+using BasicWebServer.Server.Controllers;
 using BasicWebServer.Server.HTTP;
+using SharedTrip.Contracts;
+using SharedTrip.Models;
+using SharedTrip.Models.Trips;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,9 +14,73 @@ namespace SharedTrip.Controllers
 {
     public class TripsController : Controller
     {
-        public TripsController(Request request)
+        private readonly ITripService tripService;
+        public TripsController(
+            Request request,
+            ITripService _tripService)
             : base(request)
         {
+            tripService = _tripService;
+        }
+
+        [Authorize]
+        public Response Add() => View();
+
+        [HttpPost]
+        [Authorize]
+        public Response Add(TripViewModel model)
+        {
+            var (isValid, errors) = tripService.ValidateModel(model);
+
+            if (!isValid)
+            {
+                return View(errors, "/Error");
+            }
+
+            try
+            {
+                tripService.AddTrip(model);
+            }
+            catch (Exception)
+            {
+                return View(new List<ErrorViewModel>() { new ErrorViewModel("Unexpected Error") }, "/Error");
+            }
+
+            return Redirect("/Trips/All");
+        }
+
+        [Authorize]
+        public Response All()
+        {
+            IEnumerable<TripListViewModel> trips = tripService.GetAllTrips();
+
+            return View(trips);
+        }
+
+        [Authorize]
+        public Response Details(string tripId)
+        {
+            TripDetailsViewModel tripDetailsViewModel = tripService.GetTripDetails(tripId);
+
+            return View(tripDetailsViewModel);
+        }
+
+        public Response AddUserToTrip(string tripId)
+        {
+            try
+            {
+                tripService.AddUserToTrip(tripId, User.Id);
+            }
+            catch (ArgumentException aex)
+            {
+                return View(new List<ErrorViewModel>() { new ErrorViewModel(aex.Message) }, "/Error");
+            }
+            catch (Exception)
+            {
+                return View(new List<ErrorViewModel>() { new ErrorViewModel("Unexpected Error") }, "/Error");
+            }
+
+            return Redirect("/Trips/All");
         }
     }
 }
