556dfa746e9936d7b690843bdedda2d87c469d17
Semir Baldzhiev <semir.baldzhiev@gmail.com>
2022-02-18 20:10:58 +0200

    Added trip interface

---
+++ C#-Web-Basics/ExamPreparation/Shared-Trip-2/SharedTrip/Contracts/ITripService.cs
@@ -0,0 +1,15 @@
+﻿using SharedTrip.Models;
+using SharedTrip.Models.Trips;
+using System.Collections.Generic;
+
+namespace SharedTrip.Contracts
+{
+    public interface ITripService
+    {
+        (bool isValid, IEnumerable<ErrorViewModel> errors) ValidateModel(TripViewModel model);
+        void AddTrip(TripViewModel model);
+        IEnumerable<TripListViewModel> GetAllTrips();
+        TripDetailsViewModel GetTripDetails(string tripId);
+        void AddUserToTrip(string tripId, string id);
+    }
+}
