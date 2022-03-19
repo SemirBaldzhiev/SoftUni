1a62537f0116f4411a4128747dd1f20b8bea881b
Semir Baldzhiev <semir.baldzhiev@gmail.com>
2022-03-19 22:49:02 +0200

    Added insertion sort asc and desc

---
+++ Algorithms/Sorting/InsertionSort/Program.cs
@@ -0,0 +1,48 @@
+﻿void InsertionSort(int[] arr)
+{
+    int n = arr.Length;
+    int key, j;
+
+
+    for (int i = 1; i < n; i++)
+    {
+        key = arr[i];
+        j = i - 1;
+
+        while (j >= 0 && arr[j] > key)
+        {
+            arr[j + 1] = arr[j]; 
+            j--;
+        }
+
+        arr[j + 1] = key;
+    }
+}
+
+void InsertionSortDesc(int[] arr)
+{
+    int n = arr.Length;
+    int key, j;
+
+
+    for (int i = 1; i < n; i++)
+    {
+        key = arr[i];
+        j = i - 1;
+
+        while (j >= 0 && arr[j] < key)
+        {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+
+        arr[j + 1] = key;
+    }
+}
+
+
+int[] arr = new int[] { 31, 41, 59, 26, 41, 58 };
+InsertionSort(arr);
+Console.WriteLine(String.Join(' ', arr));
+InsertionSortDesc(arr);
+Console.WriteLine(String.Join(' ', arr));
