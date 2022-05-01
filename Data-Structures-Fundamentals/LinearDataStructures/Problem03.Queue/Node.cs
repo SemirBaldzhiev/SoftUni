202002d2806f15916dfc96a7902209da9398e082
Semir Baldzhiev <semir.baldzhiev@gmail.com>
2022-05-01 13:57:01 +0300

    Added queue implementation

---
+++ Data-Structures-Fundamentals/LinearDataStructures/Problem03.Queue/Node.cs
@@ -0,0 +1,14 @@
+﻿namespace Problem03.Queue
+{
+    public class Node<T>
+    {
+        public Node(T item, Node<T> next)
+        {
+            Item = item;
+            Next = next;
+        }
+
+        public T Item { get; set; }
+        public Node<T> Next { get; set; }
+    }
+}
---
+++ Data-Structures-Fundamentals/LinearDataStructures/Problem03.Queue/Queue.cs
@@ -0,0 +1,94 @@
+﻿namespace Problem03.Queue
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+
+    public class Queue<T> : IAbstractQueue<T> where T : IComparable<T>
+    {
+        private Node<T> _head;
+
+        public int Count { get; private set; }
+
+        public bool Contains(T item)
+        {
+            var currentNode = _head;
+
+            while (currentNode != null)
+            {
+                if (currentNode.Item.CompareTo(item) == 0)
+                {
+                    return true;
+                }
+
+                currentNode = currentNode.Next;
+            }
+
+            return false;
+        }
+
+        public T Dequeue()
+        {
+            EnsureNotEmpty();
+            T itemToRem = _head.Item;
+            var newHead = _head.Next;
+
+            _head.Next = null;
+            _head = newHead;
+            --Count;
+
+            return itemToRem;
+        }
+
+        public void Enqueue(T item)
+        {
+            var newNode = new Node<T>(item, null);
+
+
+            if (_head == null)
+            {
+                _head = newNode;
+                ++Count;
+                return;
+            }
+
+            var currentNode = _head;
+            while (currentNode.Next != null)
+            {
+                currentNode = currentNode.Next;
+            }
+
+
+            currentNode.Next = newNode;
+            ++Count;
+        }
+
+        public T Peek()
+        {
+            EnsureNotEmpty();
+            return _head.Item;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            var currentNode = _head;
+
+            while (currentNode != null)
+            {
+                yield return currentNode.Item;
+                currentNode = currentNode.Next; 
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+            => GetEnumerator();
+
+        private void EnsureNotEmpty()
+        {
+            if (_head == null)
+            {
+                throw new InvalidOperationException("Empty stack");
+            }
+        }
+    }
+}
